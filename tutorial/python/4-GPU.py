1a9b8148739e28ea39f440293689f8fc48e50f8a
Matthijs Douze <matthijs@fb.com>
2017-12-18 19:23:30 +0100

    Update 4-GPU.py

--- tutorial/python/4-GPU.py
+++ tutorial/python/4-GPU.py
@@ -30,9 +30,6 @@ index.add(xb)                  # add vectors to the index
 print(index.ntotal)
 
 k = 4                          # we want to see 4 nearest neighbors
-D, I = index.search(xb[:5], k) # sanity check
-print(I)
-print(D)
 D, I = index.search(xq, k)     # actual search
 print(I[:5])                   # neighbors of the 5 first queries
 print(I[-5:])                  # neighbors of the 5 last queries
