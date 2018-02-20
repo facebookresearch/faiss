f348900d90237050964ea3246e6090b9b19a4504
Lucas Hosseini <hoss@fb.com>
2018-02-20 11:52:10 +0100

    Add IVFFlat example on GPU in python.

--- tutorial/python/4-GPU.py
+++ tutorial/python/4-GPU.py
@@ -15,20 +15,42 @@ xb[:, 0] += np.arange(nb) / 1000.
 xq = np.random.random((nq, d)).astype('float32')
 xq[:, 0] += np.arange(nq) / 1000.
 
-import faiss                   # make faiss available
+import faiss                     # make faiss available
 
 print("number of GPUs:", faiss.get_num_gpus())
 
-index = faiss.IndexFlatL2(d)   # build the index
+res = faiss.StandardGpuResources()  # use a single GPU
 
-res = faiss.StandardGpuResources()
+## Using a flat index
 
-index = faiss.index_cpu_to_gpu(res, 0, index)
+index_flat = faiss.IndexFlatL2(d)  # build a flat (CPU) index
 
-index.add(xb)                  # add vectors to the index
-print(index.ntotal)
+# make it a flat GPU index
+gpu_index_flat = faiss.index_cpu_to_gpu(res, 0, index_flat)
+
+gpu_index_flat.add(xb)         # add vectors to the index
+print(gpu_index_flat.ntotal)
 
 k = 4                          # we want to see 4 nearest neighbors
-D, I = index.search(xq, k)     # actual search
+D, I = gpu_index_flat.search(xq, k)  # actual search
+print(I[:5])                   # neighbors of the 5 first queries
+print(I[-5:])                  # neighbors of the 5 last queries
+
+
+## Using an IVF index
+
+nlist = 100
+k = 4
+quantizer = faiss.IndexFlatL2(d)  # the other index
+index_ivf = faiss.IndexIVFFlat(quantizer, d, nlist, faiss.METRIC_L2)
+# here we specify METRIC_L2, by default it performs inner-product search
+
+gpu_index_ivf = faiss.index_cpu_to_gpu(res, 0, index_ivf)
+
+gpu_index_flat.add(xb)         # add vectors to the index
+print(gpu_index_flat.ntotal)
+
+k = 4                          # we want to see 4 nearest neighbors
+D, I = gpu_index_flat.search(xq, k)  # actual search
 print(I[:5])                   # neighbors of the 5 first queries
 print(I[-5:])                  # neighbors of the 5 last queries
