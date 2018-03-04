90ee8e2f55742feb243a6c211db8ae4bfc79e8da
amrotork <amtc2018@gmail.com>
2018-03-04 11:53:29 +0200

    - Updating setup to support both mac and linux.

--- setup.py
+++ setup.py
@@ -10,16 +10,7 @@ import shutil
 import platform
 
 ## Setup files.
-here = os.path.abspath(os.path.dirname(__file__))
-makefile = os.path.join(here,"makefile.inc")
-gcc_compiler= os.path.abspath(find_executable("g++"))
-python_path = os.path.abspath(find_executable("python"))
-python_bin_folder = os.path.dirname(python_path)
-python_env = os.path.dirname(python_bin_folder)
-python_inc = get_python_inc()
-numpy_inc = get_include()
 platform_type = platform.system()
-
 if platform_type == "Darwin":
     lib_extension="dylib"
 elif platform_type == "Linux":
@@ -28,21 +19,62 @@ else:
     print("Unknown platform type: {}".format(platform_type))
     exit(1)
 
+here = os.path.abspath(os.path.dirname(__file__))
+makefile = os.path.join(here,"makefile.inc")
+python_path = os.path.abspath(find_executable("python"))
+python_bin_folder = os.path.dirname(python_path)
+python_env = os.path.dirname(python_bin_folder)
+python_inc = get_python_inc()
+numpy_inc = get_include()
 
-## Creating include file.
-with open(makefile, 'w') as outfile:
-    outfile.write("### Automatically created make include file.\n")
-    outfile.write("CC={}\n".format(gcc_compiler))
-    outfile.write("CFLAGS=-fPIC -m64 -Wall -g -O3 -mavx -msse4 -mpopcnt -fopenmp -Wno-sign-compare -std=c++11 -fopenmp\n")
-    outfile.write("LDFLAGS=-g -fPIC  -fopenmp\n")
-    outfile.write("SHAREDEXT={}\n".format(lib_extension))
-    outfile.write("SHAREDFLAGS=-shared\n")
-    outfile.write("FAISSSHAREDFLAGS=-shared\n\n\n")
-    outfile.write("MKLROOT={}\n".format(python_env))
-    outfile.write("BLASLDFLAGS=-Wl,--no-as-needed -L$(MKLROOT)/lib   -lmkl_intel_ilp64 -lmkl_core -lmkl_gnu_thread -ldl -lpthread\n")
-    outfile.write("BLASCFLAGS=-DFINTEGER=long\n\n\n")
-    outfile.write("PYTHONCFLAGS=-I{} -I{}\n".format(python_inc,numpy_inc))
-    outfile.close()
+if platform_type == "Darwin":
+    lib_extension = "dylib"
+    makefile_str = """CXX=/usr/local/opt/llvm/bin/clang++
+    CFLAGS=-fPIC -m64 -Wall -g -O3 -msse4 -mpopcnt -fopenmp -Wno-sign-compare -I/usr/local/opt/llvm/include
+    CXXFLAGS=$(CFLAGS) -std=c++11
+    LLVM_VERSION_PATH=$(shell ls -rt /usr/local/Cellar/llvm/ | tail -n1)
+    LDFLAGS=-g -fPIC -fopenmp -L/usr/local/opt/llvm/lib -L/usr/local/Cellar/llvm/${LLVM_VERSION_PATH}/lib
+    
+    
+    # common mac flags
+    SHAREDEXT=dylib
+    SHAREDFLAGS=-Wl,-F. -bundle -undefined dynamic_lookup
+    FAISSSHAREDFLAGS=-dynamiclib
+    
+    ## MKL
+    MKLROOT={}
+    BLASLDFLAGS=-Wl,--no-as-needed -L$(MKLROOT)/lib   -lmkl_intel_ilp64 -lmkl_core -lmkl_gnu_thread -ldl -lpthread
+    BLASCFLAGS=-DFINTEGER=long
+    
+    ## Python and SWIG
+    SWIGEXEC=/usr/local/bin/swig
+    PYTHONCFLAGS=-I{} -I{}    
+    """.format(python_env,python_inc,numpy_inc)
+
+    with open(makefile, 'w') as outfile:
+        outfile.write(makefile_str)
+        outfile.close()
+
+elif platform_type == "Linux":
+    gcc_compiler= os.path.abspath(find_executable("g++"))
+    lib_extension = "so"
+
+    ## Creating include file.
+    with open(makefile, 'w') as outfile:
+        outfile.write("### Automatically created make include file.\n")
+        outfile.write("CC={}\n".format(gcc_compiler))
+        outfile.write("CFLAGS=-fPIC -m64 -Wall -g -O3 -mavx -msse4 -mpopcnt -fopenmp -Wno-sign-compare -std=c++11 -fopenmp\n")
+        outfile.write("LDFLAGS=-g -fPIC  -fopenmp\n")
+        outfile.write("SHAREDEXT=so\n")
+        outfile.write("SHAREDFLAGS=-shared\n")
+        outfile.write("FAISSSHAREDFLAGS=-shared\n\n\n")
+        outfile.write("MKLROOT={}\n".format(python_env))
+        outfile.write("BLASLDFLAGS=-Wl,--no-as-needed -L$(MKLROOT)/lib   -lmkl_intel_ilp64 -lmkl_core -lmkl_gnu_thread -ldl -lpthread\n")
+        outfile.write("BLASCFLAGS=-DFINTEGER=long\n\n\n")
+        outfile.write("PYTHONCFLAGS=-I{} -I{}\n".format(python_inc,numpy_inc))
+        outfile.close()
+else:
+    print("Unknown platform {}".format(platform_type))
 
 
 ## Run make
@@ -63,7 +95,7 @@ os.mkdir("faiss")
 shutil.copyfile("python/__init__.py", "faiss/__init__.py")
 shutil.copyfile("faiss.py", "faiss/faiss.py")
 shutil.copyfile("python/swigfaiss.py", "faiss/swigfaiss.py")
-shutil.copyfile("python/_swigfaiss.so", "faiss/_swigfaiss.so")
+shutil.copyfile("python/_swigfaiss.{}".format(lib_extension), "faiss/_swigfaiss.{}".format(lib_extension))
 try:
     shutil.copyfile("python/_swigfaiss_gpu.so", "faiss/_swigfaiss_gpu.so")
     shutil.copyfile("python/swigfaiss_gpu.py", "faiss/swigfaiss_gpu.py")
@@ -92,7 +124,7 @@ setup(
     install_requires=['numpy'],
     packages=['faiss'],
     package_data={
-        'faiss': ['*.so'],
+        'faiss': ['*.{}'.format(lib_extension)],
     },
 
 )
