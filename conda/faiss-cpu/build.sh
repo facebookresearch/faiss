ce11b262f370d34ba0809309e910878e453b92d8
Lucas Hosseini <hoss@fb.com>
2018-07-09 18:36:39 +0200

    Improve build.

--- conda/faiss-cpu/build.sh
+++ conda/faiss-cpu/build.sh
@@ -1,4 +1,5 @@
-./configure
+# NOTE(beauby): This is needed as conda does not symlink `python-config`.
+./configure --with-python-config=python${PY_VER}-config
 make -j $CPU_COUNT
 cd python
 make
--- conda/faiss-cpu/run_test.py
+++
@@ -1,4 +0,0 @@
-import numpy
-import faiss
-
-faiss.Kmeans(10, 20).train(numpy.random.rand(1000, 10).astype('float32'))
