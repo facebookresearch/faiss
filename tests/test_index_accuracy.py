300c0c8519acdfcdef75c9a16871a5eb91705ae9
Lucas Hosseini <lucas.hosseini@gmail.com>
2018-12-19 07:05:33 -0800

    Fix test.

--- tests/test_index_accuracy.py
+++ tests/test_index_accuracy.py
@@ -405,8 +405,8 @@ class OPQRelativeAccuracy(unittest.TestCase):
         index_ivfpq = faiss.IndexIVFPQ(quantizer, d, ncentroids, M, 8)
         index_ivfpq.nprobe = 5
         opq_matrix = faiss.OPQMatrix(d, M)
-        opq_matrix.niter = 16
-        opq_matrix.niter_pq = 8
+        opq_matrix.niter = 20
+        opq_matrix.niter_pq = 12
         index = faiss.IndexPreTransform(opq_matrix, index_ivfpq)
 
         res = ev.launch('O+IVFPQ', index)
