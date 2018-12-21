639b7b8a774fc404dd74dcd2e9b51e80d635d470
Lucas Hosseini <hoss@fb.com>
2018-12-21 14:38:31 +0100

    Force no cuda on CPU-only version.

--- conda/faiss/build.sh
+++ conda/faiss/build.sh
@@ -1,5 +1,5 @@
 # NOTE(beauby): This is needed as conda does not symlink `python-config`.
-./configure --with-python-config=python${PY_VER}-config
+./configure --with-python-config=python${PY_VER}-config --with-cuda=/dev/null
 make -j $CPU_COUNT
 cd python
 make cpu
