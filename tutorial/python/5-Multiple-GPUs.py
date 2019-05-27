7f9072e3b1cd5cf4384d674d5a4e1981180a06e5
Lucas Hosseini <lucas.hosseini@gmail.com>
2019-05-27 02:26:56 -0700

    Fix remaining license headers.

--- tutorial/python/5-Multiple-GPUs.py
+++ tutorial/python/5-Multiple-GPUs.py
@@ -1,7 +1,6 @@
-# Copyright (c) 2015-present, Facebook, Inc.
-# All rights reserved.
+# Copyright (c) Facebook, Inc. and its affiliates.
 #
-# This source code is licensed under the BSD+Patents license found in the
+# This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
 import numpy as np
