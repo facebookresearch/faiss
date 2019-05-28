dacd5a9f51fe7933685bf906009b398ceeb504ab
Lucas Hosseini <hoss@fb.com>
2019-05-28 16:22:38 +0200

    Prepare for v1.5.2 release

--- python/setup.py
+++ python/setup.py
@@ -27,7 +27,7 @@ are implemented on the GPU. It is developed by Facebook AI Research.
 """
 setup(
     name='faiss',
-    version='1.5.1',
+    version='1.5.2',
     description='A library for efficient similarity search and clustering of dense vectors',
     long_description=long_description,
     url='https://github.com/facebookresearch/faiss',
