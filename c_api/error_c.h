842d82e2f5b86c956a8ea0fe1f5d855065b4b3cc
Eduardo Pinho <enet4mikeenet@gmail.com>
2019-06-12 11:47:33 +0100

    [c_api] Document FaissErrorCode

--- c_api/error_c.h
+++ c_api/error_c.h
@@ -16,10 +16,16 @@
 extern "C" {
 #endif
 
+/// An error code which depends on the exception thrown from the previous 
+/// operation. See `faiss_get_last_error` to retrieve the error message.
 typedef enum FaissErrorCode {
+    /// No error
     OK = 0,
+    /// Any exception other than Faiss or standard C++ library exceptions
     UNKNOWN_EXCEPT = -1,
+    /// Faiss library exception
     FAISS_EXCEPT = -2,
+    /// Standard C++ library exception
     STD_EXCEPT = -4
 } FaissErrorCode;
 
