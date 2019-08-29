a9a475b003dd03717dd57953c6105ee621180fd6
Matthijs Douze <matthijs@fb.com>
2019-08-29 15:34:14 +0200

    Update distributed_kmeans.py

--- benchs/distributed_ondisk/distributed_kmeans.py
+++ benchs/distributed_ondisk/distributed_kmeans.py
@@ -1,4 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 #! /usr/bin/env python3
+
 """
 Simple distributed kmeans implementation Relies on an abstraction
 for the training matrix, that can be sharded over several machines.
