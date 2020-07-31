a70d58cd92611a9fc872662e67be51a8862171f9
Lucas Hosseini <hoss@fb.com>
2020-08-01 01:22:09 +0200

    Fix test_index_accuracy.

--- tests/test_index_accuracy.py
+++ tests/test_index_accuracy.py
@@ -3,7 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function
 # noqa E741
 # translation of test_knn.lua
 
