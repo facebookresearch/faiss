41f3c245a0bbd05b76cf4e0503f566e0d1bec22e
Lucas Hosseini <lucas.hosseini@gmail.com>
2020-08-10 18:53:17 +0200

    [WIP] Windows CI.

---
+++ conda/faiss/build-lib.bat
@@ -0,0 +1,20 @@
+:: Copyright (c) Facebook, Inc. and its affiliates.
+::
+:: This source code is licensed under the MIT license found in the
+:: LICENSE file in the root directory of this source tree.
+
+:: Build libfaiss.so.
+cmake -B _build ^
+      -T v141 ^
+      -A x64 ^
+      -G "Visual Studio 16 2019" ^
+      -DBUILD_SHARED_LIBS=ON ^
+      -DBUILD_TESTING=OFF ^
+      -DFAISS_ENABLE_GPU=OFF ^
+      -DFAISS_ENABLE_PYTHON=OFF ^
+      -DBLA_VENDOR=Intel10_64_dyn ^
+      .
+
+cmake --build _build --config Release -j %CPU_COUNT%
+
+cmake --install _build --config Release --prefix %PREFIX%
