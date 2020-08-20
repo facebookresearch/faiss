b1f6f190efb508292251ef00468725faafb375e6
Lucas Hosseini <lucas.hosseini@gmail.com>
2020-08-20 05:13:08 +0200

    Add missing algorithm header.
    [ghstack-poisoned]

--- faiss/impl/AuxIndexStructures.cpp
+++ faiss/impl/AuxIndexStructures.cpp
@@ -7,6 +7,7 @@
 
 // -*- c++ -*-
 
+#include <algorithm>
 #include <cstring>
 
 #include <faiss/impl/AuxIndexStructures.h>
--- faiss/impl/io.cpp
+++ faiss/impl/io.cpp
@@ -7,6 +7,7 @@
 
 // -*- c++ -*-
 
+#include <algorithm>
 #include <cstring>
 #include <cassert>
 
