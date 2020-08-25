c689bd010cea9ed7daba53ace86b18ab01b7e928
Lucas Hosseini <lucas.hosseini@gmail.com>
2020-08-25 16:56:01 -0700

    Update conda packaging. (#1363)
    Summary: Pull Request resolved: https://github.com/facebookresearch/faiss/pull/1363
    
    Test Plan: Imported from OSS
    
    Reviewed By: mdouze
    
    Differential Revision: D23314731
    
    Pulled By: beauby
    
    fbshipit-source-id: b57a66cf5c44788837ee379fbb7f73adbdbf1274

---
+++ conda/faiss/install-cmake.sh
@@ -0,0 +1,10 @@
+#!/bin/sh#
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+set -e
+
+wget -O - https://github.com/Kitware/CMake/releases/download/v3.17.1/cmake-3.17.1-Linux-x86_64.tar.gz | tar xzf -
+cp -R cmake-3.17.1-Linux-x86_64/* $PREFIX
