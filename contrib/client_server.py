70eaa9b1a3ea624a7c5bc38839a0ebf8c656ab4a
Lucas Hosseini <hoss@fb.com>
2020-10-13 11:14:25 -0700

    Add missing copyright headers. (#1460)
    Summary: Pull Request resolved: https://github.com/facebookresearch/faiss/pull/1460
    
    Reviewed By: wickedfoo
    
    Differential Revision: D24278804
    
    Pulled By: beauby
    
    fbshipit-source-id: 5ea96ceb63be76a34f1eb4da03972159342cd5b6

--- contrib/client_server.py
+++ contrib/client_server.py
@@ -1,4 +1,7 @@
-#! /usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 from multiprocessing.dummy import Pool as ThreadPool
 import faiss
--- demos/demo_client_server_ivf.py
+++ demos/demo_client_server_ivf.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 import sys
 import numpy as np
