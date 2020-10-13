f92d4622c22cd89e7446abc597371a3022da60f2
Lucas Hosseini <hoss@fb.com>
2020-10-13 17:38:29 +0200

    Add missing copyright headers.

--- contrib/ondisk.py
+++ contrib/ondisk.py
@@ -1,4 +1,7 @@
-#! /usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 from typing import List
 import faiss
