7898868b864adc3ea0d14f69a7a03274114dccc0
Eyal Trabelsi <eyaltrabelsi@gmail.com>
2020-10-21 15:46:25 +0300

    Update test_gpu_index.py

--- faiss/gpu/test/test_gpu_index.py
+++ faiss/gpu/test/test_gpu_index.py
@@ -219,8 +219,8 @@ class ReferencedObject(unittest.TestCase):
 
 
 class TestShardedFlat(unittest.TestCase):
-    
-    unittest.skipIf(faiss.get_num_gpus() == 1,"relevant for multiple GPU only")
+
+    @unittest.skipIf(faiss.get_num_gpus() < 2, "Relevant for multiple GPU only.")
     def test_sharded(self):
         d = 32
         nb = 1000
