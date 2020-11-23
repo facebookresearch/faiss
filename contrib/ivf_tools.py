0b8f09c1c2d6934e613a85c064279db446e3e457
Matthijs Douze <matthijs@fb.com>
2020-11-23 07:11:09 -0800

    get_invlists seems to work with block

--- contrib/ivf_tools.py
+++ contrib/ivf_tools.py
@@ -8,6 +8,7 @@ import faiss
 
 def get_invlist(invlists, l):
     """ returns the inverted lists content. """
+    invlists = faiss.downcast_InvertedLists(invlists)
     ls = invlists.list_size(l)
     list_ids = np.zeros(ls, dtype='int64')
     ids = codes = None
@@ -15,7 +16,14 @@ def get_invlist(invlists, l):
         ids = invlists.get_ids(l)
         faiss.memcpy(faiss.swig_ptr(list_ids), ids, list_ids.nbytes)
         codes = invlists.get_codes(l)
-        list_codes = np.zeros((ls, invlists.code_size), dtype='uint8')
+        if invlists.code_size != faiss.InvertedLists.INVALID_CODE_SIZE:
+            list_codes = np.zeros((ls, invlists.code_size), dtype='uint8')
+        else:
+            # it's a BlockInvertedLists
+            npb = invlists.n_per_block
+            bs = invlists.block_size
+            ls_round = (ls + npb - 1) // npb * npb
+            list_codes = np.zeros((ls_round, bs // npb, npb), dtype='uint8')
         faiss.memcpy(faiss.swig_ptr(list_codes), codes, list_codes.nbytes)
     finally:
         if ids is not None:
