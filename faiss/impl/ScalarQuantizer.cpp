79c0e9d56ded4cfe9c10b21a5ef0ebe874b070dd
H. Vetinari <h.vetinari@gmx.com>
2021-02-11 17:51:01 +0100

    msvc doesn't seet __F16C__

--- faiss/impl/ScalarQuantizer.cpp
+++ faiss/impl/ScalarQuantizer.cpp
@@ -40,7 +40,7 @@ namespace faiss {
  ********************************************************************/
 
 #ifdef __AVX2__
-#ifdef __F16C__
+#if defined(__F16C__) || defined(_MSC_VER)
 #define USE_F16C
 #else
 #warning "Cannot enable AVX optimizations in scalar quantizer if -mf16c is not set as well"
