9a7bc175d29c2fee58aafde4799f24879fc23246
Alexander Andreev <andreevlex.as@gmail.com>
2021-04-02 20:10:16 +0300

    Add IndexScalarQuantizer for c_api

---
+++ c_api/IndexScalarQuantizer_c.cpp
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// Copyright 2004-present Facebook. All Rights Reserved.
+// -*- c++ -*-
+
+#include "IndexScalarQuantizer_c.h"
+#include <faiss/IndexScalarQuantizer.h>
+#include <faiss/impl/ScalarQuantizer.h>
+#include "macros_impl.h"
+
+using faiss::Index;
+using faiss::IndexScalarQuantizer;
+
+DEFINE_DESTRUCTOR(IndexScalarQuantizer)
+DEFINE_INDEX_DOWNCAST(IndexScalarQuantizer)
+
+int faiss_IndexScalarQuantizer_new_with(
+        FaissIndexScalarQuantizer** p_index,
+        idx_t d,
+        FaissQuantizerType qt,
+        FaissMetricType metric) {
+    try {
+        IndexScalarQuantizer* index =
+                new IndexScalarQuantizer(d, static_cast<faiss::ScalarQuantizer::QuantizerType>(qt), static_cast<faiss::MetricType>(metric));
+        *p_index = reinterpret_cast<FaissIndexScalarQuantizer*>(index);
+        return 0;
+    }
+    CATCH_AND_HANDLE
+}
