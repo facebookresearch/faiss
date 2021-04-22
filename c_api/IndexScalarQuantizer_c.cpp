d9764d8aff9ff7686ffe2ca80bb0c8a0d1c39e4a
Alexander Andreev <andreevlex.as@gmail.com>
2021-04-22 05:18:34 -0700

    Add IndexIVFScalarQuantizer for c_api (#1829)
    Summary:
    This PR add impl IndexIVFScalarQuantizer in c_api interface
    
    Pull Request resolved: https://github.com/facebookresearch/faiss/pull/1829
    
    Reviewed By: beauby
    
    Differential Revision: D27904571
    
    Pulled By: mdouze
    
    fbshipit-source-id: 2cdbbd356f7520cea897f69c90486837c569ed19

--- c_api/IndexScalarQuantizer_c.cpp
+++ c_api/IndexScalarQuantizer_c.cpp
@@ -14,6 +14,7 @@
 #include "macros_impl.h"
 
 using faiss::Index;
+using faiss::IndexIVFScalarQuantizer;
 using faiss::IndexScalarQuantizer;
 
 DEFINE_DESTRUCTOR(IndexScalarQuantizer)
@@ -39,6 +40,83 @@ int faiss_IndexScalarQuantizer_new_with(
                 static_cast<faiss::MetricType>(metric));
         *p_index = reinterpret_cast<FaissIndexScalarQuantizer*>(index);
         return 0;
+    }
+    CATCH_AND_HANDLE
+}
+
+DEFINE_DESTRUCTOR(IndexIVFScalarQuantizer)
+DEFINE_INDEX_DOWNCAST(IndexIVFScalarQuantizer)
+
+/// quantizer that maps vectors to inverted lists
+DEFINE_GETTER_PERMISSIVE(IndexIVFScalarQuantizer, FaissIndex*, quantizer)
+
+/// number of possible key values
+DEFINE_GETTER(IndexIVFScalarQuantizer, size_t, nlist)
+/// number of probes at query time
+DEFINE_GETTER(IndexIVFScalarQuantizer, size_t, nprobe)
+DEFINE_SETTER(IndexIVFScalarQuantizer, size_t, nprobe)
+
+/// whether object owns the quantizer
+DEFINE_GETTER(IndexIVFScalarQuantizer, int, own_fields)
+DEFINE_SETTER(IndexIVFScalarQuantizer, int, own_fields)
+
+int faiss_IndexIVFScalarQuantizer_new_with(
+        FaissIndexIVFScalarQuantizer** p_index,
+        FaissIndex* quantizer,
+        size_t d,
+        size_t nlist,
+        FaissQuantizerType qt) {
+    try {
+        auto q = reinterpret_cast<Index*>(quantizer);
+        auto qt_ = static_cast<faiss::ScalarQuantizer::QuantizerType>(qt);
+        IndexIVFScalarQuantizer* index =
+                new IndexIVFScalarQuantizer(q, d, nlist, qt_);
+        *p_index = reinterpret_cast<FaissIndexIVFScalarQuantizer*>(index);
+        return 0;
+    }
+    CATCH_AND_HANDLE
+}
+
+int faiss_IndexIVFScalarQuantizer_new_with_metric(
+        FaissIndexIVFScalarQuantizer** p_index,
+        FaissIndex* quantizer,
+        size_t d,
+        size_t nlist,
+        FaissQuantizerType qt,
+        FaissMetricType metric,
+        int encode_residual) {
+    try {
+        auto q = reinterpret_cast<Index*>(quantizer);
+        auto mt = static_cast<faiss::MetricType>(metric);
+        auto er = static_cast<bool>(encode_residual);
+        auto qt_ = static_cast<faiss::ScalarQuantizer::QuantizerType>(qt);
+        IndexIVFScalarQuantizer* index =
+                new IndexIVFScalarQuantizer(q, d, nlist, qt_, mt, er);
+        *p_index = reinterpret_cast<FaissIndexIVFScalarQuantizer*>(index);
+        return 0;
+    }
+    CATCH_AND_HANDLE
+}
+
+int faiss_IndexIVFScalarQuantizer_add_core(
+        FaissIndexIVFScalarQuantizer* index,
+        idx_t n,
+        const float* x,
+        const idx_t* xids,
+        const idx_t* precomputed_idx) {
+    try {
+        reinterpret_cast<IndexIVFScalarQuantizer*>(index)->add_core(
+                n, x, xids, precomputed_idx);
+    }
+    CATCH_AND_HANDLE
+}
+
+int faiss_IndexIVFScalarQuantizer_train_residual(
+        FaissIndexIVFScalarQuantizer* index,
+        idx_t n,
+        const float* x) {
+    try {
+        reinterpret_cast<IndexIVFScalarQuantizer*>(index)->train_residual(n, x);
     }
     CATCH_AND_HANDLE
 }
