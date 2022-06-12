3e79df981772ef7abcd4dbff584a89d11a52bbf8
Check Deng <checkdeng0903@gmail.com>
2022-06-12 10:48:31 +0800

    Remove unsed function

--- faiss/impl/ProductAdditiveQuantizer.h
+++ faiss/impl/ProductAdditiveQuantizer.h
@@ -58,11 +58,6 @@ struct ProductAdditiveQuantizer : AdditiveQuantizer {
     ///< Train the product additive quantizer
     void train(size_t n, const float* x) override;
 
-    void compute_codes(const float* x, uint8_t* codes, size_t n)
-            const override {
-        compute_codes_add_centroids(x, codes, n);
-    }
-
     /** Encode a set of vectors
      *
      * @param x      vectors to encode, size n * d
