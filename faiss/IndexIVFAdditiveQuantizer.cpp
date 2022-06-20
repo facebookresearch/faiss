814431d7ef5617c99caf544fbc9c63d7b91c0203
Alexandr Guzhva <aguzhva@fb.com>
2022-06-20 08:48:18 -0700

    Add sa_decode() to IndexIVFAdditiveQuantizer
    Reviewed By: mdouze
    
    Differential Revision: D37280450
    
    fbshipit-source-id: a4b66ec9bd333e70c422166f73fcb5e2eb3f36d8

--- faiss/IndexIVFAdditiveQuantizer.cpp
+++ faiss/IndexIVFAdditiveQuantizer.cpp
@@ -100,6 +100,32 @@ void IndexIVFAdditiveQuantizer::encode_vectors(
     }
 }
 
+void IndexIVFAdditiveQuantizer::sa_decode(
+        idx_t n,
+        const uint8_t* codes,
+        float* x) const {
+    const size_t coarse_size = coarse_code_size();
+
+#pragma omp parallel if (n > 1000)
+    {
+        std::vector<float> residual(d);
+
+#pragma omp for
+        for (idx_t i = 0; i < n; i++) {
+            const uint8_t* code = codes + i * (code_size + coarse_size);
+            int64_t list_no = decode_listno(code);
+            float* xi = x + i * d;
+            aq->decode(code + coarse_size, xi, 1);
+            if (by_residual) {
+                quantizer->reconstruct(list_no, residual.data());
+                for (size_t j = 0; j < d; j++) {
+                    xi[j] += residual[j];
+                }
+            }
+        }
+    }
+}
+
 IndexIVFAdditiveQuantizer::~IndexIVFAdditiveQuantizer() {}
 
 /*********************************************
