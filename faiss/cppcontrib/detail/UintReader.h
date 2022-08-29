465b2eb737951a68cb9a08fcae8591ec40cbf5e1
Alexandr Guzhva <aguzhva@fb.com>
2022-08-29 13:32:19 -0700

    Additional C++ templates for fast sa_decode
    Summary: Clean UintReader code and fix it. Add support of 10 bits.
    
    Differential Revision: D39108374
    
    fbshipit-source-id: 9570931e9bb2d75f0267009eec80959e54efbcaa

--- faiss/cppcontrib/detail/UintReader.h
+++ faiss/cppcontrib/detail/UintReader.h
@@ -1,100 +1,181 @@
 #pragma once
 
+#include <cstdint>
+
 namespace faiss {
 namespace cppcontrib {
 namespace detail {
 
 namespace {
 
-template <
-        intptr_t DIM,
-        intptr_t CODE_SIZE,
-        intptr_t CPOS,
-        bool = DIM / CODE_SIZE <= 3>
-struct Uint8ReaderImpl {
+template <intptr_t N_ELEMENTS, intptr_t CPOS>
+struct Uint8Reader {
+    static_assert(CPOS < N_ELEMENTS, "CPOS should be less than N_ELEMENTS");
+
     static intptr_t get(const uint8_t* const __restrict codes) {
-        // Read 1 byte (movzx).
-        return codes[CPOS];
-    }
-};
-template <intptr_t DIM, intptr_t CODE_SIZE, intptr_t CPOS>
-struct Uint8ReaderImpl<DIM, CODE_SIZE, CPOS, false> {
-    static intptr_t get(const uint8_t* const __restrict codes) {
-        // Read using 4-bytes.
+        // Read using 4-bytes, if possible.
         // Reading using 8-byte takes too many registers somewhy.
-        const uint32_t* __restrict codes32 =
-                reinterpret_cast<const uint32_t*>(codes);
 
         constexpr intptr_t ELEMENT_TO_READ = CPOS / 4;
         constexpr intptr_t SUB_ELEMENT = CPOS % 4;
-        const uint32_t code32 = codes32[ELEMENT_TO_READ];
 
         switch (SUB_ELEMENT) {
-            case 0:
-                return (code32 & 0x000000FF);
-            case 1:
-                return (code32 & 0x0000FF00) >> 8;
-            case 2:
-                return (code32 & 0x00FF0000) >> 16;
-            case 3:
-                return (code32) >> 24;
+            case 0: {
+                if (N_ELEMENTS > CPOS + 3) {
+                    const uint32_t code32 = *reinterpret_cast<const uint32_t*>(
+                            codes + ELEMENT_TO_READ * 4);
+                    return (code32 & 0x000000FF);
+                } else {
+                    return codes[CPOS];
+                }
+            }
+            case 1: {
+                if (N_ELEMENTS > CPOS + 2) {
+                    const uint32_t code32 = *reinterpret_cast<const uint32_t*>(
+                            codes + ELEMENT_TO_READ * 4);
+                    return (code32 & 0x0000FF00) >> 8;
+                } else {
+                    return codes[CPOS];
+                }
+            }
+            case 2: {
+                if (N_ELEMENTS > CPOS + 1) {
+                    const uint32_t code32 = *reinterpret_cast<const uint32_t*>(
+                            codes + ELEMENT_TO_READ * 4);
+                    return (code32 & 0x00FF0000) >> 16;
+                } else {
+                    return codes[CPOS];
+                }
+            }
+            case 3: {
+                if (N_ELEMENTS > CPOS) {
+                    const uint32_t code32 = *reinterpret_cast<const uint32_t*>(
+                            codes + ELEMENT_TO_READ * 4);
+                    return (code32) >> 24;
+                } else {
+                    return codes[CPOS];
+                }
+            }
         }
     }
 };
 
-template <intptr_t DIM, intptr_t CODE_SIZE, intptr_t CPOS>
-using Uint8Reader = Uint8ReaderImpl<DIM, CODE_SIZE, CPOS>;
-
 // reduces the number of read operations from RAM
-template <
-        intptr_t DIM,
-        intptr_t CODE_SIZE,
-        intptr_t CPOS,
-        bool = DIM / CODE_SIZE <= 1>
-struct Uint16ReaderImpl {
+///////////////////////////////////////////////
+// 76543210 76543210 76543210 76543210 76543210
+// 00000000 00
+//            111111 1111
+//                       2222 222222
+//                                  33 33333333
+template <intptr_t N_ELEMENTS, intptr_t CPOS>
+struct Uint10Reader {
+    static_assert(CPOS < N_ELEMENTS, "CPOS should be less than N_ELEMENTS");
+
     static intptr_t get(const uint8_t* const __restrict codes) {
-        // Read 2 bytes.
-        const uint16_t* const __restrict codesFp16 =
-                reinterpret_cast<const uint16_t*>(codes);
-        return codesFp16[CPOS];
+        // Read using 4-bytes or 2-bytes.
+
+        constexpr intptr_t ELEMENT_TO_READ = CPOS / 4;
+        constexpr intptr_t SUB_ELEMENT = CPOS % 4;
+
+        switch (SUB_ELEMENT) {
+            case 0: {
+                if (N_ELEMENTS > CPOS + 2) {
+                    const uint32_t code32 = *reinterpret_cast<const uint32_t*>(
+                            codes + ELEMENT_TO_READ * 5);
+                    return (code32 & 0b0000001111111111);
+                } else {
+                    const uint16_t code16 = *reinterpret_cast<const uint16_t*>(
+                            codes + ELEMENT_TO_READ * 5 + 0);
+                    return (code16 & 0b0000001111111111);
+                }
+            }
+            case 1: {
+                if (N_ELEMENTS > CPOS + 1) {
+                    const uint32_t code32 = *reinterpret_cast<const uint32_t*>(
+                            codes + ELEMENT_TO_READ * 5);
+                    return (code32 & 0b000011111111110000000000) >> 10;
+                } else {
+                    const uint16_t code16 = *reinterpret_cast<const uint16_t*>(
+                            codes + ELEMENT_TO_READ * 5 + 1);
+                    return (code16 & 0b0000111111111100) >> 2;
+                }
+            }
+            case 2: {
+                if (N_ELEMENTS > CPOS) {
+                    const uint32_t code32 = *reinterpret_cast<const uint32_t*>(
+                            codes + ELEMENT_TO_READ * 5);
+                    return (code32 & 0b00111111111100000000000000000000) >> 20;
+                } else {
+                    const uint16_t code16 = *reinterpret_cast<const uint16_t*>(
+                            codes + ELEMENT_TO_READ * 5 + 2);
+                    return (code16 & 0b0011111111110000) >> 4;
+                }
+            }
+            case 3: {
+                const uint16_t code16 = *reinterpret_cast<const uint16_t*>(
+                        codes + ELEMENT_TO_READ * 5 + 3);
+                return (code16 & 0b1111111111000000) >> 6;
+            }
+        }
     }
 };
-template <intptr_t DIM, intptr_t CODE_SIZE, intptr_t CPOS>
-struct Uint16ReaderImpl<DIM, CODE_SIZE, CPOS, false> {
+
+// reduces the number of read operations from RAM
+template <intptr_t N_ELEMENTS, intptr_t CPOS>
+struct Uint16Reader {
+    static_assert(CPOS < N_ELEMENTS, "CPOS should be less than N_ELEMENTS");
+
     static intptr_t get(const uint8_t* const __restrict codes) {
-        // Read using 4-bytes.
+        // Read using 4-bytes or 2-bytes.
         // Reading using 8-byte takes too many registers somewhy.
-        const uint32_t* __restrict codes32 =
-                reinterpret_cast<const uint32_t*>(codes);
 
         constexpr intptr_t ELEMENT_TO_READ = CPOS / 2;
         constexpr intptr_t SUB_ELEMENT = CPOS % 2;
-        const uint32_t code32 = codes32[ELEMENT_TO_READ];
 
         switch (SUB_ELEMENT) {
-            case 0:
-                return (code32 & 0x0000FFFF);
-            case 1:
-                return code32 >> 16;
+            case 0: {
+                if (N_ELEMENTS > CPOS + 1) {
+                    const uint32_t code32 = *reinterpret_cast<const uint32_t*>(
+                            codes + ELEMENT_TO_READ * 4);
+                    return (code32 & 0x0000FFFF);
+                } else {
+                    const uint16_t* const __restrict codesFp16 =
+                            reinterpret_cast<const uint16_t*>(codes);
+                    return codesFp16[CPOS];
+                }
+            }
+            case 1: {
+                if (N_ELEMENTS > CPOS) {
+                    const uint32_t code32 = *reinterpret_cast<const uint32_t*>(
+                            codes + ELEMENT_TO_READ * 4);
+                    return code32 >> 16;
+                } else {
+                    const uint16_t* const __restrict codesFp16 =
+                            reinterpret_cast<const uint16_t*>(codes);
+                    return codesFp16[CPOS];
+                }
+            }
         }
     }
 };
 
-template <intptr_t DIM, intptr_t CODE_SIZE, intptr_t CPOS>
-using Uint16Reader = Uint16ReaderImpl<DIM, CODE_SIZE, CPOS>;
-
 //
-template <intptr_t DIM, intptr_t CODE_SIZE, intptr_t CODE_BITS, intptr_t CPOS>
+template <intptr_t N_ELEMENTS, intptr_t CODE_BITS, intptr_t CPOS>
 struct UintReaderImplType {};
 
-template <intptr_t DIM, intptr_t CODE_SIZE, intptr_t CPOS>
-struct UintReaderImplType<DIM, CODE_SIZE, 8, CPOS> {
-    using reader_type = Uint8Reader<DIM, CODE_SIZE, CPOS>;
+template <intptr_t N_ELEMENTS, intptr_t CPOS>
+struct UintReaderImplType<N_ELEMENTS, 8, CPOS> {
+    using reader_type = Uint8Reader<N_ELEMENTS, CPOS>;
 };
 
-template <intptr_t DIM, intptr_t CODE_SIZE, intptr_t CPOS>
-struct UintReaderImplType<DIM, CODE_SIZE, 16, CPOS> {
-    using reader_type = Uint16Reader<DIM, CODE_SIZE, CPOS>;
+template <intptr_t N_ELEMENTS, intptr_t CPOS>
+struct UintReaderImplType<N_ELEMENTS, 10, CPOS> {
+    using reader_type = Uint10Reader<N_ELEMENTS, CPOS>;
+};
+
+template <intptr_t N_ELEMENTS, intptr_t CPOS>
+struct UintReaderImplType<N_ELEMENTS, 16, CPOS> {
+    using reader_type = Uint16Reader<N_ELEMENTS, CPOS>;
 };
 
 } // namespace
@@ -102,8 +183,12 @@ struct UintReaderImplType<DIM, CODE_SIZE, 16, CPOS> {
 // reduces the number of read operations from RAM
 template <intptr_t DIM, intptr_t CODE_SIZE, intptr_t CODE_BITS, intptr_t CPOS>
 using UintReader =
-        typename UintReaderImplType<DIM, CODE_SIZE, CODE_BITS, CPOS>::
+        typename UintReaderImplType<DIM / CODE_SIZE, CODE_BITS, CPOS>::
                 reader_type;
+
+template <intptr_t N_ELEMENTS, intptr_t CODE_BITS, intptr_t CPOS>
+using UintReaderRaw =
+        typename UintReaderImplType<N_ELEMENTS, CODE_BITS, CPOS>::reader_type;
 
 } // namespace detail
 } // namespace cppcontrib
