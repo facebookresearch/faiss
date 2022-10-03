c5b49b79df57cab7b7890c28f0ee5cb7329cbddd
Matthijs Douze <matthijs@fb.com>
2022-10-03 11:45:41 -0700

    split __init__.py into subsections (#2508)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/faiss/pull/2508
    
    the Faiss python module was in a monolythic __init__.py
    This diff splits it in several sub-modules.
    The tricky thing is to make inter-dependencies work.
    
    Reviewed By: alexanderguzhva
    
    Differential Revision: D39969794
    
    fbshipit-source-id: 6e7f896a4b35a7c1a0a1f3a986daa32a00bfae6b

--- faiss/python/setup.py
+++ faiss/python/setup.py
@@ -15,6 +15,10 @@ os.mkdir("faiss")
 shutil.copytree("contrib", "faiss/contrib")
 shutil.copyfile("__init__.py", "faiss/__init__.py")
 shutil.copyfile("loader.py", "faiss/loader.py")
+shutil.copyfile("class_wrappers.py", "faiss/class_wrappers.py")
+shutil.copyfile("gpu_wrappers.py", "faiss/gpu_wrappers.py")
+shutil.copyfile("extra_wrappers.py", "faiss/extra_wrappers.py")
+shutil.copyfile("array_conversions.py", "faiss/array_conversions.py")
 
 ext = ".pyd" if platform.system() == 'Windows' else ".so"
 prefix = "Release/" * (platform.system() == 'Windows')
