a5ce19b3fa43da24a4b19bd2c30f41b255cb5484
Alexandr Guzhva <aguzhva@meta.com>
2023-02-07 12:43:58 -0800

    OSS legal requirements (#2698)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/faiss/pull/2698
    
    Add headers about copyright.
    
    Reviewed By: algoriddle
    
    Differential Revision: D43085637
    
    fbshipit-source-id: 8b5e4ac7e29062a8adfc9543dec790b06c6f0748

--- faiss/utils/distances_fused/distances_fused.cpp
+++ faiss/utils/distances_fused/distances_fused.cpp
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 #include <faiss/utils/distances_fused/distances_fused.h>
 
 #include <faiss/impl/platform_macros.h>
--- tests/test_cppcontrib_uintreader.cpp
+++ tests/test_cppcontrib_uintreader.cpp
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 // This test was designed to be run using valgrind or ASAN to test the
 // correctness of memory accesses.
 
