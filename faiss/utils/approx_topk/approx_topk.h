491373fc7014186fa14696f4e73ad294ee3261b3
aguzhva <aguzhva@fb.com>
2023-03-28 08:26:29 -0700

    expose approx top-k mode to a separate header file
    Differential Revision: https://internalfb.com/D44398351
    
    fbshipit-source-id: afd4ed424cba54ea7c0f69e62e13e6875338617a

--- faiss/utils/approx_topk/approx_topk.h
+++ faiss/utils/approx_topk/approx_topk.h
@@ -74,31 +74,8 @@
 
 #include <faiss/impl/platform_macros.h>
 
-/// Represents the mode of use of approximate top-k computations
-/// that allows to trade accuracy vs speed. So, every options
-/// besides EXACT_TOPK increases the speed.
-///
-/// B represents the number of buckets.
-/// D is the number of min-k elements to track within every bucket.
-///
-/// Default option is EXACT_TOPK.
-/// APPROX_TOPK_BUCKETS_B16_D2 is worth starting from, if you'd like
-/// to experiment a bit.
-///
-/// It seems that only the limited number of combinations are
-/// meaningful, because of the limited supply of SIMD registers.
-/// Also, certain combinations, such as B32_D1 and B16_D1, were concluded
-/// to be not very precise in benchmarks, so ones were not introduced.
-///
-/// TODO: Consider d-ary SIMD heap.
-
-enum ApproxTopK_mode_t : int {
-    EXACT_TOPK = 0,
-    APPROX_TOPK_BUCKETS_B32_D2 = 1,
-    APPROX_TOPK_BUCKETS_B8_D3 = 2,
-    APPROX_TOPK_BUCKETS_B16_D2 = 3,
-    APPROX_TOPK_BUCKETS_B8_D2 = 4,
-};
+// the list of available modes is in the following file
+#include <faiss/utils/approx_topk/mode.h>
 
 #ifdef __AVX2__
 #include <faiss/utils/approx_topk/avx2-inl.h>
