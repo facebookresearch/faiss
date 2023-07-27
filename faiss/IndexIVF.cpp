0906b086ea480f973df52224f5f40c5e1d8cea36
andriygav <andriy.graboviy@mail.ru>
2023-07-27 14:18:07 +0300

    ifx linting

--- faiss/IndexIVF.cpp
+++ faiss/IndexIVF.cpp
@@ -180,7 +180,7 @@ void IndexIVF::add(idx_t n, const float* x) {
 
 void IndexIVF::add_with_ids(idx_t n, const float* x, const idx_t* xids) {
     std::unique_ptr<idx_t[]> coarse_idx(new idx_t[n]);
-    
+
     idx_t bs = 256;
 #pragma omp parallel for
     for (idx_t i0 = 0; i0 < n; i0 += bs) {
