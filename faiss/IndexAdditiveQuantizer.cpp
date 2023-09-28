0f182519a689f51f10a940ee0c4389eee6e55b94
generatedunixname89002005325676 <generatedunixname89002005325676@meta.com>
2023-09-28 08:26:29 -0700

    Daily `arc lint --take CLANGFORMAT`
    Reviewed By: bigfootjon
    
    Differential Revision: D49724184
    
    fbshipit-source-id: dae2af01c245516e910801e11acda3fdd8bfaa54

--- faiss/IndexAdditiveQuantizer.cpp
+++ faiss/IndexAdditiveQuantizer.cpp
@@ -560,7 +560,12 @@ void ResidualCoarseQuantizer::search(
         }
         for (idx_t i0 = 0; i0 < n; i0 += bs) {
             idx_t i1 = std::min(n, i0 + bs);
-            search(i1 - i0, x + i0 * d, k, distances + i0 * k, labels + i0 * k, params_in);
+            search(i1 - i0,
+                   x + i0 * d,
+                   k,
+                   distances + i0 * k,
+                   labels + i0 * k,
+                   params_in);
             InterruptCallback::check();
         }
         return;
