d3187155fa51525fd70225cecb37acb817809821
Alexandr Guzhva <alexanderguzhva@gmail.com>
2023-11-28 13:09:48 -0500

    Introduce avx512 optimization mode and FAISS_OPT_LEVEL env variable
    Signed-off-by: Alexandr Guzhva <alexanderguzhva@gmail.com>

--- faiss/python/loader.py
+++ faiss/python/loader.py
@@ -18,7 +18,7 @@ def supported_instruction_sets():
 
     Example:
     >>> supported_instruction_sets()  # for x86
-    {"SSE2", "AVX2", ...}
+    {"SSE2", "AVX2", "AVX512", ...}
     >>> supported_instruction_sets()  # for PPC
     {"VSX", "VSX2", ...}
     >>> supported_instruction_sets()  # for ARM
@@ -41,25 +41,58 @@ def supported_instruction_sets():
             return {"AVX2"}
     elif platform.system() == "Linux":
         import numpy.distutils.cpuinfo
+        result = set()
         if "avx2" in numpy.distutils.cpuinfo.cpu.info[0].get('flags', ""):
-            return {"AVX2"}
+            result.add("AVX2")
+        if "avx512" in numpy.distutils.cpuinfo.cpu.info[0].get('flags', ""):
+            result.add("AVX512")
+        return result
     return set()
 
 
 logger = logging.getLogger(__name__)
 
-has_AVX2 = "AVX2" in supported_instruction_sets()
-if has_AVX2:
+instruction_sets = None
+
+# try to load optimization level from env variable
+opt_env_variable_name = "FAISS_OPT_LEVEL"
+opt_level = os.environ.get(opt_env_variable_name, None)
+
+if opt_level is None:
+    logger.debug(f"Environment variable {opt_env_variable_name} is not set, " \
+                "so let's pick the instruction set according to the current CPU")
+    instruction_sets = supported_instruction_sets()
+else:
+    logger.debug(f"Using {opt_level} as an instruction set.")
+    instruction_sets = set()
+    instruction_sets.add(opt_level)
+
+loaded = False
+has_AVX512 = any("AVX512" in x.upper() for x in instruction_sets)
+if has_AVX512:
+    try:
+        logger.info("Loading faiss with AVX512 support.")
+        from .swigfaiss_avx512 import *
+        logger.info("Successfully loaded faiss with AVX512 support.")
+        loaded = True
+    except ImportError as e:
+        logger.info(f"Could not load library with AVX512 support due to:\n{e!r}")
+        # reset so that we load without AVX512 below
+        loaded = False
+
+has_AVX2 = "AVX2" in instruction_sets
+if has_AVX2 and not loaded:
     try:
         logger.info("Loading faiss with AVX2 support.")
         from .swigfaiss_avx2 import *
         logger.info("Successfully loaded faiss with AVX2 support.")
+        loaded = True
     except ImportError as e:
         logger.info(f"Could not load library with AVX2 support due to:\n{e!r}")
         # reset so that we load without AVX2 below
-        has_AVX2 = False
+        loaded = False
 
-if not has_AVX2:
+if not loaded:
     # we import * so that the symbol X can be accessed as faiss.X
     logger.info("Loading faiss.")
     from .swigfaiss import *
