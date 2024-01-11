0013c702f47bedbf6159ac356e61f378ccd12ac8
Gergely Szilvasy <gsz@meta.com>
2024-01-11 08:26:33 -0800

    avx512 CI + conda packages (#3197)
    Summary: Pull Request resolved: https://github.com/facebookresearch/faiss/pull/3197
    
    Reviewed By: mlomeli1
    
    Differential Revision: D52689379
    
    Pulled By: algoriddle
    
    fbshipit-source-id: 54e27c6d310d6da14777ae10ae62f46e7076cacf

--- conda/faiss-gpu-raft/build-pkg.sh
+++ conda/faiss-gpu-raft/build-pkg.sh
@@ -7,17 +7,17 @@
 set -e
 
 
-# Build swigfaiss.so/swigfaiss_avx2.so.
+# Build swigfaiss.so/swigfaiss_avx2.so/swigfaiss_avx512.so
 cmake -B _build_python_${PY_VER} \
       -Dfaiss_ROOT=_libfaiss_stage/ \
-      -DFAISS_OPT_LEVEL=avx2 \
+      -DFAISS_OPT_LEVEL=avx512 \
       -DFAISS_ENABLE_GPU=ON \
       -DFAISS_ENABLE_RAFT=ON \
       -DCMAKE_BUILD_TYPE=Release \
       -DPython_EXECUTABLE=$PYTHON \
       faiss/python
 
-make -C _build_python_${PY_VER} -j$(nproc) swigfaiss swigfaiss_avx2
+make -C _build_python_${PY_VER} -j$(nproc) swigfaiss swigfaiss_avx2 swigfaiss_avx512
 
 # Build actual python module.
 cd _build_python_${PY_VER}/
