c9a23a5c7c182603805d54251b235273ffefa8ec
Tarang Jain <jaintarang2015@gmail.com>
2024-01-17 01:14:01 -0800

    update benchmarking, testing scripts

--- benchs/bench_ivfflat_raft.py
+++ benchs/bench_ivfflat_raft.py
@@ -9,7 +9,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     h ttp://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,6 +22,7 @@ import numpy as np
 import faiss
 import time
 import argparse
+import rmm
 
 ######################################################
 # Command-line parsing
@@ -64,6 +65,9 @@ rs = np.random.RandomState(123)
 
 res = faiss.StandardGpuResources()
 
+# Use an RMM pool memory resource for device allocations
+mr = rmm.mr.PoolMemoryResource(rmm.mr.CudaMemoryResource())
+rmm.mr.set_current_device_resource(mr)
 
 def bench_train_milliseconds(index, trainVecs, use_raft):
     co = faiss.GpuMultipleClonerOptions()
--- faiss/gpu/test/TestGpuIndexIVFFlat.cpp
+++ faiss/gpu/test/TestGpuIndexIVFFlat.cpp
@@ -738,6 +738,13 @@ TEST(TestGpuIndexIVFFlat, UnifiedMemory) {
             0.015f);
 
 #if defined USE_NVIDIA_RAFT
+    // Update the RMM device resource for RAFT internal allocations to use managed memory
+    rmm::mr::device_memory_resource* old_mr =
+            rmm::mr::get_per_device_resource(cuda_device_id{device});
+    rmm::mr::managed_memory_resource managed_mr;
+    // Updates the current device resource pointer to `managed_mr`
+    rmm::mr::set_per_device_resource(cuda_device_id{device}, &managed_mr);
+
     config.use_raft = true;
     config.indicesOptions = faiss::gpu::INDICES_64_BIT;
     faiss::gpu::GpuIndexIVFFlat raftGpuIndex(
@@ -755,6 +762,9 @@ TEST(TestGpuIndexIVFFlat, UnifiedMemory) {
             kF32MaxRelErr,
             0.1f,
             0.015f);
+
+    // reset the RMM memory resource to the old value
+    rmm::mr::set_per_device_resource(cuda_device_id{device}, old_mr);
 #endif
 }
 
