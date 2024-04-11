e3eb21c6de3dcd2893a20d03078bb37f31685805
Junjie Qi <jqi@meta.com>
2024-04-11 15:25:16 -0700

    revert some part

--- faiss/utils/simdlib_neon.h
+++ faiss/utils/simdlib_neon.h
@@ -168,18 +168,10 @@ static inline std::string elements_to_string(const char* fmt, const S& simd) {
     simd.store(bytes);
     char res[1000], *ptr = res;
     for (size_t i = 0; i < N; ++i) {
-        int bytesWritten = snprintf(ptr, sizeof(res) - (ptr - res), fmt, bytes[i]);
-        if (bytesWritten >= 0) {
-            ptr += bytesWritten;
-        } else {
-            // Handle error
-            break;
-        }
+        ptr += sprintf(ptr, fmt, bytes[i]);
     }
     // strip last ,
-    if (ptr != res) {
-        ptr[-1] = 0;
-    }
+    ptr[-1] = 0;
     return std::string(res);
 }
 
