037ff71b35364f9f7ee1c9ca2380c5913653462a
ramilbakhshyiev <7597197+ramilbakhshyiev@users.noreply.github.com>
2024-06-15 19:15:41 +0000

    only build generic to speed feedback loop

--- conda/faiss/build-lib.sh
+++ conda/faiss/build-lib.sh
@@ -11,14 +11,14 @@ set -e
 cmake -B _build \
       -DBUILD_SHARED_LIBS=ON \
       -DBUILD_TESTING=OFF \
-      -DFAISS_OPT_LEVEL=avx512 \
+      -DFAISS_OPT_LEVEL=generic \
       -DFAISS_ENABLE_GPU=OFF \
       -DFAISS_ENABLE_PYTHON=OFF \
       -DBLA_VENDOR=Intel10_64lp \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release .
 
-make -C _build -j$(nproc) faiss faiss_avx2 faiss_avx512
+make -C _build -j$(nproc) faiss
 
 cmake --install _build --prefix $PREFIX
 cmake --install _build --prefix _libfaiss_stage/
