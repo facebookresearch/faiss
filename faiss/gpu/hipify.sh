611d5aeb34d125b0b2167f584a810291480aa423
ramilbakhshyiev <7597197+ramilbakhshyiev@users.noreply.github.com>
2024-07-24 08:53:03 +0000

    properly find deps, disable ssh

--- faiss/gpu/hipify.sh
+++ faiss/gpu/hipify.sh
@@ -218,3 +218,5 @@ sudo ln -s /home/runner/miniconda3/x86_64-conda-linux-gnu/sysroot/usr/lib64/libm
 sudo ln -s /home/runner/miniconda3/x86_64-conda-linux-gnu/sysroot/lib64/libmvec.so.1 /lib64/libmvec.so.1
 sudo ln -s /home/runner/miniconda3/x86_64-conda-linux-gnu/sysroot/lib64/libc.so.6 /lib64/libc.so.6
 sudo ln -s /home/runner/miniconda3/x86_64-conda-linux-gnu/sysroot/usr/lib64/libc_nonshared.a /usr/lib64/libc_nonshared.a
+sudo ln -s /home/runner/miniconda3/x86_64-conda-linux-gnu/sysroot/lib64/libpthread.so.0 /lib64/libpthread.so.0
+sudo ln -s /home/runner/miniconda3/x86_64-conda-linux-gnu/sysroot/usr/lib64/libpthread_nonshared.a /usr/lib64/libpthread_nonshared.a
