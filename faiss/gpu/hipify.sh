6fe4640d5cfbbebd2a3b40b688415f3a83289f47
Kumar Saurabh Arora <kuarora@meta.com>
2024-09-09 22:58:05 -0700

    Fixing headers as per OSS requirement (#3847)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/faiss/pull/3847
    
    same as title.
    Fixing headers as raised in task - P1558157110
    
    Reviewed By: junjieqi
    
    Differential Revision: D62408917
    
    fbshipit-source-id: 652b55dd2ba9617edeb2b05172be0f42291d7035

--- faiss/gpu/hipify.sh
+++ faiss/gpu/hipify.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 # go one level up from faiss/gpu
 top=$(dirname "${BASH_SOURCE[0]}")/..
