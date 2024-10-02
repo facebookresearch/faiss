be54c305462971147835f9569b4bae62c9051f9b
Matthijs Douze <matthijs@meta.com>
2024-10-02 07:51:06 -0700

    add test for external modules (#3699)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/faiss/pull/3699
    
    External modules should work fine as plug-ins to Faiss in the following cases:
    
    * additional objects that can be passed in as callbacks to Faiss
    
    * functions that use `faiss.swig_ptr` to pass in arrays.
    
    The `swig_ptr` functionality does not always work well (also depending on the platform). Therefore this diff adds a small external swig file to test that everything works smoothly on that end.
    
    Differential Revision: D60379753

---
+++ tests/test_external_module.py
@@ -0,0 +1,66 @@
+
+import unittest
+
+import numpy as np
+
+import faiss
+try:
+    import faiss_example_external_module as external_module 
+except ModuleNotFoundError:
+    external_module = None 
+
+@unittest.skipIf(external_module is None, "cannot load external_module")
+class TestCustomIDSelector(unittest.TestCase):
+    """ test if we can construct a custom IDSelector """
+
+    def test_IDSelector(self):
+        ids = external_module.IDSelectorModulo(3)
+        self.assertFalse(ids.is_member(1))
+        self.assertTrue(ids.is_member(3))
+
+
+@unittest.skipIf(external_module is None, "cannot load external_module")
+class TestArrayConversions(unittest.TestCase):
+
+    def test_idx_array(self):
+        tab = np.arange(10).astype('int64')
+        new_sum = external_module.sum_of_idx(len(tab), faiss.swig_ptr(tab))
+        self.assertEqual(new_sum, tab.sum())
+
+    def do_array_test(self, ty):
+        tab = np.arange(10).astype(ty)
+        func = getattr(external_module, 'sum_of_' + ty)
+        print("perceived type", faiss.swig_ptr(tab))
+        new_sum = func(len(tab), faiss.swig_ptr(tab))
+        self.assertEqual(new_sum, tab.sum())
+
+    def test_sum_uint8(self):
+        self.do_array_test('uint8')
+
+    def test_sum_uint16(self):
+        self.do_array_test('uint16')
+
+    def test_sum_uint32(self):
+        self.do_array_test('uint32')
+
+    def test_sum_uint64(self):
+        self.do_array_test('uint64')
+
+    # this conversion does not work
+    def test_sum_int8(self):
+        self.do_array_test('int8')
+
+    def test_sum_int16(self):
+        self.do_array_test('int16')
+
+    def test_sum_int32(self):
+        self.do_array_test('int32')
+
+    def test_sum_int64(self):
+        self.do_array_test('int64')
+
+    def test_sum_float32(self):
+        self.do_array_test('float32')
+
+    def test_sum_float64(self):
+        self.do_array_test('float64')
