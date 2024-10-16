0c6415093a5164f09f41855cf7763f170ebfeff3
Michael Norris <mnorris@meta.com>
2024-10-16 12:11:49 -0700

    Resolve "duplicate-license-header": Find and replace duplicate license headers (#3967)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/faiss/pull/3967
    
    This diff just finds and replaces duplicate license headers.
    
    See the errors for "duplicate-license-header" in D64429711 under "linter-coverage-verification" signal.
    
    Differential Revision: D64484123

--- c_api/Clustering_c.h
+++ c_api/Clustering_c.h
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// Copyright 2004-present Facebook. All Rights Reserved
 // -*- c -*-
 
 #ifndef FAISS_CLUSTERING_C_H
--- c_api/index_factory_c.cpp
+++ c_api/index_factory_c.cpp
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-// Copyright 2004-present Facebook. All Rights Reserved.
 // -*- c++ -*-
 
 #include "index_factory_c.h"
