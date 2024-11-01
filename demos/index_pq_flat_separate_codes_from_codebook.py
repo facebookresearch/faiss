fcf0ff81da45018bdec6ed490f4a41dd043f3b36
Amir Sadoughi <amir.sadoughi@gmail.com>
2024-11-01 16:34:13 -0400

    Update index_pq_flat_separate_codes_from_codebook.py

--- demos/index_pq_flat_separate_codes_from_codebook.py
+++ demos/index_pq_flat_separate_codes_from_codebook.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env -S grimaldi --kernel bento_kernel_faiss
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 # fmt: off
 # flake8: noqa
 
