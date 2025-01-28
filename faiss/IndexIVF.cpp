2eab136aaf8169171cdbb497ac9d49b7cab4fd4d
Derrick Quinn <dq55@cornell.edu>
2025-01-28 16:07:38 -0500

    Update IVF main loop

--- faiss/IndexIVF.cpp
+++ faiss/IndexIVF.cpp
@@ -696,6 +696,54 @@ void IndexIVF::search_preassigned(
             for (int64_t i = 0; i < n; i++) {
                 reorder_result(distances + i * k, labels + i * k);
             }
+
+        } else if (pmode == 4) {
+            /// for this parallelization method, we parallelize over every
+            /// existing list, group together all the queries affiliated, and
+            /// scan all the queries of this list in one go.
+#pragma omp single
+            {
+                keys_by_list.resize(nlist);
+                for (idx_t i = 0; i < nlist; i++) {
+                    keys_by_list[i].reserve(nprobe * n / nlist);
+                }
+                for (idx_t il = 0; il < n; il++) {
+                    for (idx_t ikey = 0; ikey < nprobe; ikey++) {
+                        keys_by_list[keys[il * nprobe + ikey]].emplace_back(il);
+                    }
+                }
+                if (metric_type == METRIC_INNER_PRODUCT) {
+                    heap_heapify<HeapForIP>(k * n, distances, labels);
+                } else {
+                    heap_heapify<HeapForL2>(k * n, distances, labels);
+                }
+            }
+#pragma omp for schedule(dynamic)
+            for (idx_t il = 0; il < invlists->nlist; il++) {
+                std::vector<idx_t>& my_list = keys_by_list[il];
+                idx_t num_of_x = my_list.size();
+                if (num_of_x > 0) {
+                    std::vector<idx_t> local_idx(k * num_of_x);
+                    std::vector<float> local_dis(k * num_of_x);
+                    scanner->set_query_batched(x, my_list);
+                    ndis += scan_one_list(
+                            il, 0, local_dis.data(), local_idx.data(), unlimited_list_size);
+#pragma omp critical
+                    for (idx_t one_x = 0; one_x < num_of_x; one_x++) {
+                        float* simi = distances + my_list[one_x] * k;
+                        idx_t* idxi = labels + my_list[one_x] * k;
+                        add_local_results(
+                                local_dis.data() + one_x * k,
+                                local_idx.data() + one_x * k,
+                                simi,
+                                idxi);
+                    }
+                }
+            }
+#pragma omp for schedule(dynamic)
+            for (idx_t i = 0; i < n; i++) {
+                reorder_result(distances + i * k, labels + i * k);
+            }
         } else {
             FAISS_THROW_FMT("parallel_mode %d not supported\n", pmode);
         }
