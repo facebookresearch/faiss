cd797c7c38af7311dcbe47a04a2a8234319f8497
Tarang Jain <jaintarang2015@gmail.com>
2025-01-28 17:11:18 -0800

    Benchmarking Scripts for cuVS Index, more docs updates (#4117)
    Summary:
    - Small updates to FAISS + cuVS docs.
    - Fixes IVF-Flat and IVF-PQ benchmarking scripts.
    - Re-enable cuVS backend in gpu python tests
    
    Pull Request resolved: https://github.com/facebookresearch/faiss/pull/4117
    
    Reviewed By: mnorris11
    
    Differential Revision: D68792968
    
    Pulled By: asadoughi
    
    fbshipit-source-id: 36cd0bbd51635bc559144e9d5733dc29e6f7a229

--- benchs/bench_ivfpq_cuvs.py
+++ benchs/bench_ivfpq_cuvs.py
@@ -1,9 +1,10 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
+# @lint-ignore-every LICENSELINT
+# Copyright (c) Meta Platforms, Inc. and its affiliates.
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 #
-# Copyright (c) 2024, NVIDIA CORPORATION.
+# Copyright (c) 2024-2025, NVIDIA CORPORATION.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,6 +24,31 @@ import faiss
 import time
 import argparse
 import rmm
+import ctypes
+
+try:
+    from faiss.contrib.datasets_fb import \
+        DatasetSIFT1M, DatasetDeep1B, DatasetBigANN
+except ImportError:
+    from faiss.contrib.datasets import \
+        DatasetSIFT1M, DatasetDeep1B, DatasetBigANN
+
+
+# ds = DatasetDeep1B(10**6)
+# ds = DatasetBigANN(nb_M=1)
+ds = DatasetSIFT1M()
+
+xq = ds.get_queries()
+xb = ds.get_database()
+gt = ds.get_groundtruth()
+
+xt = ds.get_train()
+
+nb, d = xb.shape
+nq, d = xq.shape
+nt, d = xt.shape
+
+M = d / 2
 
 ######################################################
 # Command-line parsing
@@ -30,32 +56,39 @@ import rmm
 
 parser = argparse.ArgumentParser()
 
-from datasets import load_sift1M, evaluate
-
-
-print("load data")
-xb, xq, xt, gt = load_sift1M()
 
 def aa(*args, **kwargs):
     group.add_argument(*args, **kwargs)
 
 
 group = parser.add_argument_group('benchmarking options')
-aa('--cuvs_only', default=False, action='store_true',
-   help='whether to only produce cuVS enabled benchmarks')
+
+aa('--bm_train', default=True,
+   help='whether to benchmark train operation on GPU index')
+aa('--bm_add', default=True,
+   help='whether to benchmark add operation on GPU index')
+aa('--bm_search', default=True,
+   help='whether to benchmark search operation on GPU index')
+
 
 group = parser.add_argument_group('IVF options')
-aa('--bits_per_code', default=8, type=int, help='bits per code. Note that < 8 is only supported when cuVS is enabled')
-aa('--pq_len', default=2, type=int, help='number of vector elements represented by one PQ code')
-aa('--use_precomputed', default=True, type=bool, help='use precomputed codes (not with cuVS enabled)')
+aa('--nlist', default=1024, type=np.int64,
+    help="number of IVF centroids")
+aa('--bits_per_code', default=8, type=np.int64, help='bits per code. Note that < 8 is only supported when cuVS is enabled')
+
 
 group = parser.add_argument_group('searching')
+
 aa('--k', default=10, type=int, help='nb of nearest neighbors')
-aa('--nprobe', default=50, type=int, help='nb of IVF lists to probe')
+aa('--nprobe', default=10, help='nb of IVF lists to probe')
 
 args = parser.parse_args()
 
 print("args:", args)
+
+gt = gt[:, :args.k]
+nlist = args.nlist
+bits_per_code = args.bits_per_code
 
 rs = np.random.RandomState(123)
 
@@ -65,104 +98,90 @@ res = faiss.StandardGpuResources()
 mr = rmm.mr.PoolMemoryResource(rmm.mr.CudaMemoryResource())
 rmm.mr.set_current_device_resource(mr)
 
-# A heuristic to select a suitable number of lists
-def compute_nlist(numVecs):
-    nlist = np.sqrt(numVecs)
-    if (numVecs / nlist < 1000):
-        nlist = numVecs / 1000
-    return int(nlist)
+
+def eval_recall(neighbors, t):
+    speed = t * 1000 / nq
+    qps = 1000 / speed
+
+    corrects = (gt == neighbors).sum()
+    recall = corrects / (nq * args.k)
+
+    return recall, qps
 
 
-def bench_train_milliseconds(index, trainVecs, use_cuvs):
-    co = faiss.GpuMultipleClonerOptions()
-    # use float 16 lookup tables to save space
-    co.useFloat16LookupTables = True
-    co.use_cuvs = use_cuvs
-    index_gpu = faiss.index_cpu_to_gpu(res, 0, index, co)
+def bench_train_milliseconds(trainVecs, use_cuvs):
+    config = faiss.GpuIndexIVFPQConfig()
+    config.use_cuvs = use_cuvs
+    index = faiss.GpuIndexIVFPQ(res, d, 1024, 32, 8, faiss.METRIC_L2, config)
     t0 = time.time()
-    index_gpu.train(trainVecs)
+    index.train(trainVecs)
     return 1000*(time.time() - t0)
 
-n_rows, n_cols = xb.shape
-n_train, _ = xt.shape
-M = n_cols // args.pq_len
-nlist = compute_nlist(n_rows)
-index = faiss.index_factory(n_cols, "IVF{},PQ{}x{}np".format(nlist, M, args.bits_per_code))
 
-print("=" * 40)
-print("GPU Train Benchmarks")
-print("=" * 40)
-cuvs_gpu_train_time = bench_train_milliseconds(index, xt, True)
-if args.cuvs_only:
-    print("Method: IVFPQ, Operation: TRAIN, dim: %d, n_centroids %d, numSubQuantizers %d, bitsPerCode %d, numTrain: %d, cuVS enabled GPU train time: %.3f milliseconds" % (
-        n_cols, nlist, M, args.bits_per_code, n_train, cuvs_gpu_train_time))
-else:
-    classical_gpu_train_time = bench_train_milliseconds(
-        index, xt, False)
-    print("Method: IVFPQ, Operation: TRAIN, dim: %d, n_centroids %d, numSubQuantizers %d, bitsPerCode %d, numTrain: %d, classical GPU train time: %.3f milliseconds, cuVS enabled GPU train time: %.3f milliseconds" % (
-        n_cols, nlist, M, args.bits_per_code, n_train, classical_gpu_train_time, cuvs_gpu_train_time))
+#warmup
+xw = rs.rand(nt, d)
+bench_train_milliseconds(xw, True)
 
 
-def bench_add_milliseconds(index, addVecs, use_cuvs):
-    co = faiss.GpuMultipleClonerOptions()
-    # use float 16 lookup tables to save space
-    co.useFloat16LookupTables = True
-    co.use_cuvs = use_cuvs
-    index_gpu = faiss.index_cpu_to_gpu(res, 0, index, co)
-    index_gpu.copyFrom(index)
+if args.bm_train:
+    print("=" * 40)
+    print("GPU Train Benchmarks")
+    print("=" * 40)
+
+    cuvs_gpu_train_time = bench_train_milliseconds(xt, True)
+    classical_gpu_train_time = bench_train_milliseconds(xt, False)
+    print("TRAIN, dim: %d, nlist %d, numTrain: %d, classical GPU train time: %.3f milliseconds, cuVS enabled GPU train time: %.3f milliseconds" % (
+        d, nlist, nt, classical_gpu_train_time, cuvs_gpu_train_time))
+
+
+def bench_add_milliseconds(addVecs, index_cpu, use_cuvs):
+    # construct a GPU index using the same trained coarse quantizer
+    config = faiss.GpuClonerOptions()
+    config.use_cuvs = use_cuvs
+    index_gpu = faiss.index_cpu_to_gpu(res, 0, index_cpu, config)
+    assert(index_gpu.is_trained)
     t0 = time.time()
     index_gpu.add(addVecs)
     return 1000*(time.time() - t0)
 
-print("=" * 40)
-print("GPU Add Benchmarks")
-print("=" * 40)
-index.train(xt)
-cuvs_gpu_add_time = bench_add_milliseconds(index, xb, True)
-if args.cuvs_only:
-    print("Method: IVFPQ, Operation: ADD, dim: %d, n_centroids %d numSubQuantizers %d, bitsPerCode %d, numAdd %d, cuVS enabled GPU add time: %.3f milliseconds" % (
-        n_cols, nlist, M, args.bits_per_code, n_rows, cuvs_gpu_add_time))
-else:
-    classical_gpu_add_time = bench_add_milliseconds(
-        index, xb, False)
-    print("Method: IVFFPQ, Operation: ADD, dim: %d, n_centroids %d, numSubQuantizers %d, bitsPerCode %d, numAdd %d, classical GPU add time: %.3f milliseconds, cuVS enabled GPU add time: %.3f milliseconds" % (
-        n_cols, nlist, M, args.bits_per_code, n_rows, classical_gpu_add_time, cuvs_gpu_add_time))
+
+if args.bm_add:
+    print("=" * 40)
+    print("GPU Add Benchmarks")
+    print("=" * 40)
+    quantizer = faiss.IndexFlatL2(d)
+    index_cpu = faiss.IndexIVFPQ(quantizer, d, 1024, 32, 8, faiss.METRIC_L2)
+    index_cpu.train(xt)
+    cuvs_gpu_add_time = bench_add_milliseconds(xb, index_cpu, True)
+    classical_gpu_add_time = bench_add_milliseconds(xb, index_cpu, False)
+    print("ADD, dim: %d, nlist %d, numAdd: %d, classical GPU add time: %.3f milliseconds, cuVS enabled GPU add time: %.3f milliseconds" % (
+        d, nlist, nb, classical_gpu_add_time, cuvs_gpu_add_time))
 
 
-def bench_search_milliseconds(index, addVecs, queryVecs, nprobe, k, use_cuvs):
-    co = faiss.GpuMultipleClonerOptions()
+def bench_search_milliseconds(index, queryVecs, nprobe, k, use_cuvs):
+    co = faiss.GpuClonerOptions()
     co.use_cuvs = use_cuvs
-    co.useFloat16LookupTables = True
     index_gpu = faiss.index_cpu_to_gpu(res, 0, index, co)
-    index_gpu.copyFrom(index)
-    index_gpu.add(addVecs)
     index_gpu.nprobe = nprobe
     t0 = time.time()
-    index_gpu.search(queryVecs, k)
-    return 1000*(time.time() - t0)
+    _, I = index_gpu.search(queryVecs, k)
+    return I, 1000*(time.time() - t0)
 
 
+# Search benchmarks: both indexes have identical IVF centroids and lists. 
 if args.bm_search:
     print("=" * 40)
     print("GPU Search Benchmarks")
     print("=" * 40)
-    queryset_sizes = [1, 10, 100, 1000, 10000]
-    n_train, n_cols = xt.shape
-    n_add, _ = xb.shape
-    print(xq.shape)
-    M = n_cols // args.pq_len
-    nlist = compute_nlist(n_add)
-    index = faiss.index_factory(n_cols, "IVF{},PQ{}x{}np".format(nlist, M, args.bits_per_code))
-    index.train(xt)
-    for n_rows in queryset_sizes:
-        queryVecs = xq[np.random.choice(xq.shape[0], n_rows, replace=False)]
-        cuvs_gpu_search_time = bench_search_milliseconds(
-            index, xb, queryVecs, args.nprobe, args.k, True)
-        if args.cuvs_only:
-            print("Method: IVFPQ, Operation: SEARCH, dim: %d, n_centroids: %d, numSubQuantizers %d, bitsPerCode %d, numVecs: %d, numQuery: %d, nprobe: %d, k: %d, cuVS enabled GPU search time: %.3f milliseconds" % (
-                n_cols, nlist, M, args.bits_per_code, n_add, n_rows, args.nprobe, args.k, cuvs_gpu_search_time))
-        else:
-            classical_gpu_search_time = bench_search_milliseconds(
-                index, xb, queryVecs, args.nprobe, args.k, False)
-            print("Method: IVFPQ, Operation: SEARCH, dim: %d, n_centroids: %d, numSubQuantizers %d, bitsPerCode %d, numVecs: %d, numQuery: %d, nprobe: %d, k: %d, classical GPU search time: %.3f milliseconds, cuVS enabled GPU search time: %.3f milliseconds" % (
-                n_cols, nlist, M, args.bits_per_code, n_add, n_rows, args.nprobe, args.k, classical_gpu_search_time, cuvs_gpu_search_time))
+    index_cpu = faiss.IndexIVFPQ(quantizer, d, 1024, 32, 8, faiss.METRIC_L2)
+    index_cpu.train(xt)
+    index_cpu.add(xb)
+
+    cuvs_indices, cuvs_gpu_search_time = bench_search_milliseconds(
+        index_cpu, xq, args.nprobe, args.k, True)
+    classical_gpu_indices, classical_gpu_search_time = bench_search_milliseconds(
+        index_cpu, xq, args.nprobe, args.k, False)
+    cuvs_recall, cuvs_qps = eval_recall(cuvs_indices, cuvs_gpu_search_time)
+    classical_recall, classical_qps = eval_recall(classical_gpu_indices, classical_gpu_search_time)
+    print("SEARCH, dim: %d, nlist: %d, numVecs: %d, numQuery: %d, nprobe: %d, k: %d, classical GPU qps: %.3f, cuVS enabled GPU qps: %.3f"  % (
+        d, nlist, nb, nq, args.nprobe, args.k, classical_qps, cuvs_qps))
