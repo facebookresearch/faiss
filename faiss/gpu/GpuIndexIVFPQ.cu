69f2a5c80d90db2b6cffb02728fcb680574abb63
wwq <wenchingw@outlook.com>
2025-03-17 10:55:15 +0800

    fix: IVFPQ of raft/cuvs does not require pq length check for Faiss' original implementation

--- faiss/gpu/GpuIndexIVFPQ.cu
+++ faiss/gpu/GpuIndexIVFPQ.cu
@@ -568,15 +568,14 @@ void GpuIndexIVFPQ::verifyPQSettings_() const {
         }
     }
 
-    // The number of bytes per encoded vector must be one we support
-    FAISS_THROW_IF_NOT_FMT(
-            ivfpqConfig_.interleavedLayout ||
-                    IVFPQ::isSupportedPQCodeLength(subQuantizers_),
-            "Number of bytes per encoded vector / sub-quantizers (%d) "
-            "is not supported",
-            subQuantizers_);
-
     if (!should_use_cuvs(config_)) {
+        // The number of bytes per encoded vector must be one we support
+        FAISS_THROW_IF_NOT_FMT(
+                ivfpqConfig_.interleavedLayout ||
+                        IVFPQ::isSupportedPQCodeLength(subQuantizers_),
+                "Number of bytes per encoded vector / sub-quantizers (%d) "
+                "is not supported",
+                subQuantizers_);
         // Sub-quantizers must evenly divide dimensions available
         FAISS_THROW_IF_NOT_FMT(
                 this->d % subQuantizers_ == 0,
