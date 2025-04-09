2a8e497b6f4e6b8e225b2e8a049b42b595bf3571
Andy Lee <andylizf@outlook.com>
2025-04-09 08:33:56 +0000

    build script

---
+++ demo/build_demo.py
@@ -0,0 +1,40 @@
+import faiss
+import numpy as np
+
+M = 32
+efSearch = 32  # number of entry points (neighbors) we use on each layer
+efConstruction = 32  # number of entry points used on each layer
+                     # during construction
+
+
+# now define a function to read the fvecs file format of Sift1M dataset
+def read_fvecs(fp):
+    a = np.fromfile(fp, dtype='int32')
+    d = a[0]
+    return a.reshape(-1, d + 1)[:, 1:].copy().view('float32')
+
+# 1M samples
+xb = read_fvecs('./sift/sift_base.fvecs')
+# queries
+xq = read_fvecs('./sift/sift_query.fvecs')[0].reshape(1, -1)
+xq_full = read_fvecs('./sift/sift_query.fvecs')
+print(xb.shape)
+
+print('building index')
+index = faiss.IndexHNSWFlat(xb.shape[1], M)
+index.hnsw.efConstruction = efConstruction
+index.add(xb)
+
+print('searching')
+index.hnsw.efSearch = efSearch
+D, I = index.search(xq, 10)
+print(I)
+
+levels = faiss.vector_to_array(index.hnsw.levels)
+print(levels)
+
+cum_nneighbor_per_level = faiss.vector_to_array(index.hnsw.cum_nneighbor_per_level)
+print(cum_nneighbor_per_level)
+
+offsets = faiss.vector_to_array(index.hnsw.offsets)
+print(offsets)
