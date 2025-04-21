8483e050b752e28412ecdff4eeb7ef64385e2731
Andy Lee <andylizf@outlook.com>
2025-04-21 08:34:28 +0000

    simple build, using sampled 1M rpj_wiki

--- demo/simple_build.py
+++ demo/simple_build.py
@@ -1,5 +1,8 @@
+import time
 import faiss
 import numpy as np
+import pickle
+import os
 
 M = 32
 efSearch = 32  # number of entry points (neighbors) we use on each layer
@@ -7,35 +10,46 @@ efConstruction = 256  # number of entry points used on each layer
                      # during construction
 
 
-# now define a function to read the fvecs file format of Sift1M dataset
-def read_fvecs(fp):
-    a = np.fromfile(fp, dtype='int32')
-    d = a[0]
-    return a.reshape(-1, d + 1)[:, 1:].copy().view('float32')
-
-SIFT_DIR = "/home/ubuntu/Power-RAG/HNSW/HNSWfaiss/sift"
+EMBEDDING_FILE = "/powerrag/scaling_out/embeddings/facebook/contriever-msmarco/rpj_wiki_1M/1-shards/passages_00.pkl"
 # 1M samples
-xb = read_fvecs(SIFT_DIR + '/sift_base.fvecs')
-# queries
-xq = read_fvecs(SIFT_DIR + '/sift_query.fvecs')[0].reshape(1, -1)
-xq_full = read_fvecs(SIFT_DIR + '/sift_query.fvecs')
-xq_1 = xq[0].reshape(1, xq.shape[1])
-print(xb.shape)
+print(f"Loading embeddings from {EMBEDDING_FILE}...")
+with open(EMBEDDING_FILE, 'rb') as f:
+    data = pickle.load(f)
+
+xb = data[1]
+print(f"Original dtype: {xb.dtype}")
+
+if xb.dtype != np.float32:
+    print("Converting embeddings to float32.")
+    xb = xb.astype(np.float32)
+else:
+    print("Embeddings are already float32.")
+print(f"Loaded database embeddings (xb), shape: {xb.shape}")
+d = xb.shape[1] # Get dimension
+
+# 1000 queries
+num_queries = 1000
+if num_queries > xb.shape[0]:
+    print(f"Warning: Requested {num_queries} queries, but only {xb.shape[0]} vectors available. Using all vectors as queries.")
+    num_queries = xb.shape[0]
+
+print(f"Using the first {num_queries} vectors from the database as queries (xq_full).")
+xq_full = xb[:num_queries]
+print(f"Query embeddings (xq_full), shape: {xq_full.shape}")
 
 
 # recall_idx = []
 
-index_flat = faiss.IndexFlatL2(xb.shape[1])
+index_flat = faiss.IndexFlatL2(d)
 index_flat.add(xb)
 D_flat, recall_idx_flat = index_flat.search(xq_full[:1000], k=3)
 
 print(recall_idx_flat)
 
 print('building index')
-index = faiss.IndexHNSWFlat(xb.shape[1], 32, faiss.METRIC_L2, 32)
+index = faiss.IndexHNSWFlat(d, 32, faiss.METRIC_L2, 32)
 index.hnsw.efConstruction = efConstruction
 index.add(xb)
-import time
 
 for efSearch in [2, 4, 8, 16, 32, 64]:
     index.efSearch = efSearch
@@ -43,7 +57,7 @@ for efSearch in [2, 4, 8, 16, 32, 64]:
     index.hnsw.efSearch = efSearch
     # calculate the time of searching
     start_time = time.time()
-    D, I = index.search(xq_full[:1000], 3)
+    D, I = index.timech(xq_full[:1000], 3)
     end_time = time.time()
     print(f'time: {end_time - start_time}')
     # print(I)
