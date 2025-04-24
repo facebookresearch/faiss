2f360939ed6ccc4d9b0bb6f076a41e5e4c1514f7
Michael Norris <mnorris@meta.com>
2025-04-24 09:34:11 -0700

    Increment next release, v1.11.0 (#4308)
    Summary:
    
    Increment to next release of Faiss, v1.11.0
    
    Differential Revision: D73595484

--- faiss/python/setup.py
+++ faiss/python/setup.py
@@ -105,7 +105,7 @@ are implemented on the GPU. It is developed by Facebook AI Research.
 """
 setup(
     name="faiss",
-    version="1.10.0",
+    version="1.11.0",
     description="A library for efficient similarity search and clustering of dense vectors",
     long_description=long_description,
     url="https://github.com/facebookresearch/faiss",
