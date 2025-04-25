972ed134aa1441611f149de490dc8c6864dd3310
yichuan520030910320 <yichuanjiaoda@sjtu.edu.cn>
2025-04-25 23:21:33 +0000

    add build

--- demo/simple_build.py
+++ demo/simple_build.py
@@ -127,7 +127,7 @@ D_flat, recall_idx_flat = index_flat.search(xq_full, k=K_NEIGHBORS)
 print(recall_idx_flat)
 
 # Create a specific directory for this index configuration
-index_dir = f"{INDEX_SAVING_FILE}/5p_hnsw_IP_M{M}_efC{efConstruction}"
+index_dir = f"{INDEX_SAVING_FILE}/0.5_hnsw_IP_M{M}_efC{efConstruction}"
 os.makedirs(index_dir, exist_ok=True)
 index_filename = f"{index_dir}/index.faiss"
 
