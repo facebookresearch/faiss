91da020ce87aa0ab8388831ef97127c8102eec8d
Aditi Ahuja <aditi.ahuja@couchbase.com>
2025-05-26 14:36:58 +0530

    C API for binary search with params

--- c_api/IndexBinary_c.h
+++ c_api/IndexBinary_c.h
@@ -94,6 +94,27 @@ int faiss_IndexBinary_search(
 
 /** query n vectors of dimension d to the index.
  *
+ * return at most k vectors. If there are not enough results for a
+ * query, the result array is padded with -1s.
+ *
+ * @param index       opaque pointer to index object
+ * @param x           input vectors to search, size n * d
+ * @param k           number of results to return
+ * @param params      search parameters
+ * @param distances   output distances, size n*k
+ * @param labels      output labels of the NNs, size n*k
+ */
+int faiss_IndexBinary_search_with_params(
+        const FaissIndexBinary* index,
+        idx_t n,
+        const uint8_t* x,
+        idx_t k,
+        const FaissSearchParameters* params,
+        int32_t* distances,
+        idx_t* labels);
+
+/** query n vectors of dimension d to the index.
+ *
  * return all vectors with distance < radius. Note that many
  * indexes do not implement the range_search (only the k-NN search
  * is mandatory).
