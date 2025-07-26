bed2a154162d8f96c597e7688f0e3dd7b80797c8
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2025-07-26 15:45:34 -0700

    fbcode//faiss:faiss (#4462)
    Summary: Pull Request resolved: https://github.com/facebookresearch/faiss/pull/4462
    
    Reviewed By: dtolnay
    
    Differential Revision: D79041872
    
    fbshipit-source-id: 5150bacf3d109e16cf2c6af59ff536e388e2742d

--- faiss/impl/HNSW.cpp
+++ faiss/impl/HNSW.cpp
@@ -144,7 +144,7 @@ void HNSW::print_neighbor_stats(int level) const {
                         n_reciprocal++;
                         continue;
                     }
-                    if (neighset.count(i3)) {
+                    if (neighset.contains(i3)) {
                         neighset.erase(i3);
                         n_common++;
                     }
