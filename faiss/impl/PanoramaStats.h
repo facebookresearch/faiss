95555e11210e50317097343f1793a6984f5ce4df
Akash Nayar <akashknayar5@gmail.com>
2025-10-19 02:48:59 +0000

    Add Faiss header

--- faiss/impl/PanoramaStats.h
+++ faiss/impl/PanoramaStats.h
@@ -1,4 +1,12 @@
-// faiss/impl/PanoramaStats.h
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// -*- c++ -*-
+
 #ifndef FAISS_PANORAMA_STATS_H
 #define FAISS_PANORAMA_STATS_H
 
