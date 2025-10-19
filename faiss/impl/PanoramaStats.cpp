32c0f113e277bf365fba666c8973539ddfb87ccc
Akash Nayar <akashknayar5@gmail.com>
2025-10-19 18:30:38 +0000

    Fix undefined behavior

--- faiss/impl/PanoramaStats.cpp
+++ faiss/impl/PanoramaStats.cpp
@@ -20,7 +20,11 @@ void PanoramaStats::reset() {
 void PanoramaStats::add(const PanoramaStats& other) {
     total_dims_scanned += other.total_dims_scanned;
     total_dims += other.total_dims;
-    ratio_dims_scanned = static_cast<float>(total_dims_scanned) / total_dims;
+    if (total_dims > 0) {
+        ratio_dims_scanned = static_cast<float>(total_dims_scanned) / total_dims;
+    } else {
+        ratio_dims_scanned = 1.0f;
+    }
 }
 
 PanoramaStats indexPanorama_stats;
