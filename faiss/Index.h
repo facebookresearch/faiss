3c54e2efb891b1a317e69308097ba54122c3b033
Michael Norris <mnorris@meta.com>
2025-11-12 03:26:01 -0800

    Increment to next release, v1.13.0 (#4673)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/faiss/pull/4673
    
    Prior one: D79863512
    
    Wiki: https://www.internalfb.com/wiki/Vector_Search/Onboarding/Faiss_Contributor/Release/Release_Process/
    
    Reviewed By: subhadeepkaran
    
    Differential Revision: D86789918
    
    fbshipit-source-id: cbbff50cb3af43b5b75c760e32175d5a285ed712

--- faiss/Index.h
+++ faiss/Index.h
@@ -17,7 +17,7 @@
 #include <sstream>
 
 #define FAISS_VERSION_MAJOR 1
-#define FAISS_VERSION_MINOR 12
+#define FAISS_VERSION_MINOR 13
 #define FAISS_VERSION_PATCH 0
 
 // Macro to combine the version components into a single string
