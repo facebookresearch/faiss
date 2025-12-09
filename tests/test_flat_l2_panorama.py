d1d554d205d489a4d307d91f860dffd026e01ac2
Akash Nayar <akashknayar5@gmail.com>
2025-12-09 21:31:16 +0000

    rebase

--- tests/test_flat_l2_panorama.py
+++ tests/test_flat_l2_panorama.py
@@ -15,7 +15,6 @@ Paper: https://www.arxiv.org/pdf/2510.00566
 """
 
 import unittest
-import os
 
 import faiss
 import numpy as np
@@ -572,10 +571,8 @@ class TestIndexFlatL2Panorama(unittest.TestCase):
         index = self.create_panorama(d, nlevels, xb)
 
         D_before, I_before = index.search(xq, k)
-        faiss.write_index(index, "index.bin")
-        index_after = faiss.read_index("index.bin")
+        index_after = faiss.deserialize_index(faiss.serialize_index(index))
         D_after, I_after = index_after.search(xq, k)
-        os.unlink("index.bin")
 
         np.testing.assert_array_equal(I_before, I_after)
         np.testing.assert_array_equal(D_before, D_after)
