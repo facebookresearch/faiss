491fe678a127a9caeb9c861483cbdb356125c74b
Tarang Jain <40517122+tarang-jain@users.noreply.github.com>
2026-02-03 16:47:04 +0530

    Use branch main for rapids-cmake

--- cmake/thirdparty/fetch_rapids.cmake
+++ cmake/thirdparty/fetch_rapids.cmake
@@ -19,7 +19,7 @@ set(RAPIDS_VERSION "26.02")
 set(rapids-cmake-version ${RAPIDS_VERSION})
 
 if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/FAISS_RAPIDS.cmake)
-    file(DOWNLOAD https://raw.githubusercontent.com/rapidsai/rapids-cmake/branch-${RAPIDS_VERSION}/RAPIDS.cmake
+    file(DOWNLOAD https://raw.githubusercontent.com/rapidsai/rapids-cmake/main/RAPIDS.cmake
             ${CMAKE_CURRENT_BINARY_DIR}/FAISS_RAPIDS.cmake)
 endif()
 include(${CMAKE_CURRENT_BINARY_DIR}/FAISS_RAPIDS.cmake)
