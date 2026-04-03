e55a6570981b8a550979b9f4fd73f23f286e6fe8
Akash Nayar <akashknayar5@gmail.com>
2026-04-03 22:09:06 +0000

    Correct dims scanned calculation

--- faiss/impl/Panorama.h
+++ faiss/impl/Panorama.h
@@ -356,9 +356,10 @@ size_t Panorama::progressive_filter_batch(
     size_t total_active = num_active;
     bool first_level_direct = (num_active == curr_batch_size);
 
+    local_stats.total_dims += total_active * n_levels;
+
     for (size_t level = 0; (level < n_levels) && (num_active > 0); level++) {
         local_stats.total_dims_scanned += num_active;
-        local_stats.total_dims += total_active;
 
         float query_cum_norm = query_cum_sums[level + 1];
 
