f9be206dd71ef07dc455f16aa381f075457fc69f
Jure Bajic <jure.bajic94@gmail.com>
2026-05-25 16:03:24 +0200

    Adapt python api

--- tests/test_autotune.py
+++ tests/test_autotune.py
@@ -5,9 +5,12 @@
 
 
 import unittest
+
+import numpy as np
 import faiss
 
 from common_faiss_tests import for_all_simd_levels
+from faiss.contrib import datasets
 
 
 @for_all_simd_levels
@@ -48,3 +51,42 @@ class TestParameterSpace(unittest.TestCase):
         ps.set_index_parameter(index, "quantizer_efSearch", 5)
         index2 = faiss.downcast_index(index.quantizer)
         self.assertEqual(index2.hnsw.efSearch, 5)
+
+    def test_explore_with_params(self):
+        """SearchParameters forwarded through explore restricts results."""
+        d = 32
+        ds = datasets.SyntheticDataset(d, 1000, 200, 50)
+        index = faiss.index_factory(d, "IVF32,Flat")
+        index.train(ds.get_train())
+        index.add(ds.get_database())
+
+        xq = ds.get_queries()
+        k = 10
+
+        gt_D, gt_I = index.search(xq, k)
+        crit = faiss.OneRecallAtRCriterion(ds.nq, k)
+        crit.set_groundtruth(gt_D, gt_I)
+
+        ps = faiss.ParameterSpace()
+        ps.initialize(index)
+        ps.n_experiments = 0
+        ps.verbose = 0
+
+        def best_perf(ops):
+            n = ops.optimal_pts.size()
+            return ops.optimal_pts.at(n - 1).perf
+
+        # without params: should achieve high recall
+        ops_full = ps.explore(index, xq, crit)
+        self.assertGreater(best_perf(ops_full), 0.9)
+
+        # exclude all ground-truth IDs: recall must drop to 0
+        gt_ids = set(int(v) for v in gt_I.flatten() if v >= 0)
+        non_gt = np.array(
+            [i for i in range(ds.nb) if i not in gt_ids],
+            dtype='int64'
+        )
+        sel = faiss.IDSelectorBatch(non_gt)
+        params = faiss.SearchParametersIVF(sel=sel)
+        ops_sel = ps.explore(index, xq, crit, params=params)
+        self.assertEqual(best_perf(ops_sel), 0.0)
