e794ab336c3b6ffaebd10d19a78a159ee8a68beb
Jure Bajic <jure.bajic94@gmail.com>
2026-05-26 15:32:04 +0200

    Add tests

--- tests/test_build_blocks.py
+++ tests/test_build_blocks.py
@@ -664,3 +664,41 @@ class TestMapInt64ToInt64(unittest.TestCase):
     def xx_test_large(self):
         # don't run by default because it's slow
         self.do_test(2 ** 21, 10 ** 6)
+
+
+class TestRanklistIntersectionSize(unittest.TestCase):
+
+    def _intersect(self, v1, v2):
+        a = np.array(v1, dtype='int64')
+        b = np.array(v2, dtype='int64')
+        return faiss.ranklist_intersection_size(
+            len(a), faiss.swig_ptr(a), len(b), faiss.swig_ptr(b)
+        )
+
+    def test_basic_intersection(self):
+        self.assertEqual(self._intersect([1, 2, 3], [2, 3, 4]), 2)
+
+    def test_no_overlap(self):
+        self.assertEqual(self._intersect([1, 2, 3], [4, 5, 6]), 0)
+
+    def test_full_overlap(self):
+        self.assertEqual(self._intersect([1, 2, 3], [1, 2, 3]), 3)
+
+    def test_symmetric(self):
+        # result must be the same regardless of which list is larger
+        self.assertEqual(
+            self._intersect([1, 2, 3, 4], [2, 3]),
+            self._intersect([2, 3], [1, 2, 3, 4]),
+        )
+
+    def test_duplicates_in_v2_not_double_counted(self):
+        # 1 appears twice in v2 but should count only once
+        self.assertEqual(self._intersect([1, 2], [1, 1, 2]), 2)
+
+    def test_duplicates_in_v1_not_double_counted(self):
+        # 1 appears twice in v1 but the same v2 entry should not count twice
+        self.assertEqual(self._intersect([1, 1, 2], [1, 2]), 2)
+
+    def test_empty_lists(self):
+        self.assertEqual(self._intersect([], [1, 2, 3]), 0)
+        self.assertEqual(self._intersect([1, 2, 3], []), 0)
