1ea656433a97be689642a31d7b2b7dd477f50d41
anupamme <mediratta@gmail.com>
2026-08-05 09:12:17 +0000

    fix: python.lang.security.audit.dangerous-system-call-tainted-env-args.dangerous-system-call-tainted-env-args security vulnerability
    Automated security fix generated by OrbisAI Security

--- benchs/bench_all_ivf/cmp_with_scann.py
+++ benchs/bench_all_ivf/cmp_with_scann.py
@@ -6,6 +6,8 @@
 import time
 import sys
 import os
+import shlex
+import shutil
 import argparse
 
 import numpy as np
@@ -91,7 +93,8 @@ def main():
         ds = load_dataset(args.db, download=args.download)
         print(ds)
         # store for SCANN
-        os.system(f"rm -rf {cache_dir}; mkdir -p {cache_dir}")
+        shutil.rmtree(cache_dir, ignore_errors=True)
+        os.makedirs(cache_dir, exist_ok=True)
         tosave = {
             "xb": ds.get_database(),
             "xq": ds.get_queries(),
@@ -102,7 +105,8 @@ def main():
             print("save", fname)
             np.save(fname, v)
 
-        open(cache_dir + "metric", "w").write(ds.metric)
+        with open(os.path.join(os.path.realpath(cache_dir), "metric"), "w") as f:
+            f.write(ds.metric)
 
     dataset = {}
     for kn in "xb xq gt".split():
@@ -112,7 +116,8 @@ def main():
     xb = dataset["xb"]
     xq = dataset["xq"]
     gt = dataset["gt"]
-    distance_measure = open(cache_dir + "metric").read()
+    with open(os.path.join(os.path.realpath(cache_dir), "metric")) as f:
+        distance_measure = f.read()
 
     if args.lib == "faiss":
         import faiss
@@ -179,16 +184,16 @@ def main():
     if args.lib != "scann" and args.thenscann:
         # just append --lib scann, that will override the previous cmdline
         # options
-        cmdline = " ".join(sys.argv) + " --lib scann"
-        cmdline = (
+        scann_args = sys.argv + ["--lib", "scann"]
+        shell_cmd = (
             ". ~/anaconda3/etc/profile.d/conda.sh ; "
-            + "conda activate scann_1.1.1; "
-            "python -u " + cmdline
+            "conda activate scann_1.1.1; "
+            "python -u " + " ".join(shlex.quote(a) for a in scann_args)
         )
 
-        print("running", cmdline)
+        print("running", shell_cmd)
 
-        os.system(cmdline)
+        os.system(shell_cmd)  # nosec B605 - args are sanitized via shlex.quote
 
 
 ###############################################################
@@ -221,7 +226,8 @@ def scann_make_index(xb, distance_measure, scann_dir, reorder_k):
 
     print("write index to", scann_dir)
 
-    os.system(f"rm -rf {scann_dir}; mkdir -p {scann_dir}")
+    shutil.rmtree(scann_dir, ignore_errors=True)
+    os.makedirs(scann_dir, exist_ok=True)
     # os.mkdir(scann_dir)
     searcher.serialize(scann_dir)
     return searcher
